48c8e5966b6b0b7265efde278b7921e6bb417c83
w-dee <w-dee@users.noreply.github.com>
2004-07-18 15:37:44 +0000

    TVPConstAlphaBlend_SD_a_c 修正
    git-svn-id: svn://localhost/kirikiri2/branches/2.23@112 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- visual/glgen/gengl.pl
+++ visual/glgen/gengl.pl
@@ -484,16 +484,16 @@ static tjs_uint32 TVP_INLINE_FUNC TVPAdditiveBlend_a_d_o(tjs_uint32 dest, tjs_ui
 /* TVPAdditiveBlend_d_a is not yet implemented because the expression may loose precision. */
 
 
-static tjs_uint32 TVP_INLINE_FUNC TVPBlendARGB(tjs_uint32 a, tjs_uint32 b, tjs_int ratio)
+static tjs_uint32 TVP_INLINE_FUNC TVPBlendARGB(tjs_uint32 b, tjs_uint32 a, tjs_int ratio)
 {
 	/* returns a * ratio + b * (1 - ratio) */
 	tjs_uint32 b2;
 	tjs_uint32 t;
 	b2 = b & 0x00ff00ff;
-	t = (b2 + ((a & 0x00ff00ff) - b2) * ratio >> 8) & 0x00ff00ff;
+	t = (b2 + (((a & 0x00ff00ff) - b2) * ratio >> 8)) & 0x00ff00ff;
 	b2 = (b & 0xff00ff00) >> 8;
 	return t + 
-		((b2 + (((a & 0xff00ff00)>>8) - b2) * ratio) & 0xff00ff00);
+		(((b2 + (( ((a & 0xff00ff00) >> 8) - b2) * ratio >> 8)) << 8)& 0xff00ff00);
 }
 
 
