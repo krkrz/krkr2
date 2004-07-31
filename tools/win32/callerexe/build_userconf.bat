b373d9a9fc113e00878c9daa30e914a33629bb73
w-dee <w-dee@users.noreply.github.com>
2004-07-31 02:59:38 +0000

    userconf.exeをkirikiri2\bin\win32\enduser-toolsに出力するように
    git-svn-id: svn://localhost/kirikiri2/branches/2.23/kirikiri2/src@246 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- tools/win32/callerexe/build_userconf.bat
+++ tools/win32/callerexe/build_userconf.bat
@@ -3,6 +3,6 @@ bcc32 -O2 -v -c userconf_main.c
 tasm32 /zi /ml userconf.asm
 ilink32 -v -aa -Tpe userconf.OBJ userconf_main.obj, userconf.exe, , import32.lib, , userconf.RES
 
-copy userconf.exe ..\..\..\..\bin\win32
-copy userconf.tds ..\..\..\..\bin\win32
+copy userconf.exe ..\..\..\..\bin\win32\enduser-tools
+copy userconf.tds ..\..\..\..\bin\win32\enduser-tools
 
