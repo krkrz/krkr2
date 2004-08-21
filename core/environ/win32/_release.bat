4c1de9ac88dbebdb97c7dc56e7443525b0cb2c0b
w-dee <w-dee@users.noreply.github.com>
2004-08-21 15:01:23 +0000

    リリース用バッチファイルでkrkr.exeが正常に生成されなかったのを修正
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src@333 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- core/environ/win32/_release.bat
+++ core/environ/win32/_release.bat
@@ -21,6 +21,7 @@ call gen_optdesc_res.bat
 
 cd %TOP_DIR%\environ\win32
 copy tvpwin32.bpr tvpwin32.~bpr /y
+copy tvpwin32.res tvpwin32.~res /y
 perl basefix.pl
 
 cd %TOP_DIR%\msg
@@ -30,9 +31,13 @@ perl update_svn_revision.pl
 cd %TOP_DIR%\environ\win32
 "c:\Program Files\Borland\CBuilder5\Bin\bcb" tvpwin32.bpr -b -omakeout.txt
 
-call krkrcp.bat
+cd %TOP_DIR%\..\..\bin\win32
+del krkr.eXe /Q
+copy /b tvpwin32.exe+"%TOP_DIR%\environ\win32\optionarea.bin" krkr.eXe /y
 
+cd %TOP_DIR%\environ\win32
 copy tvpwin32.~bpr tvpwin32.bpr /y
+copy tvpwin32.~res tvpwin32.res /y
 
 cd %TOP_DIR%\msg
 copy svn_revision.~h svn_revision.h /y
