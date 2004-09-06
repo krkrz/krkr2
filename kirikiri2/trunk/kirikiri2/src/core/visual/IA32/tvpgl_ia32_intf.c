c7c458c3b50cfd0d05475990e43f1dbec224de16
w-dee <w-dee@users.noreply.github.com>
2004-09-06 15:49:06 +0000

    TVPInterpStretchConstAlphaBlend_mmx_a 実装
    git-svn-id: svn://localhost@407 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/core/visual/IA32/tvpgl_ia32_intf.c
+++ kirikiri2/trunk/kirikiri2/src/core/visual/IA32/tvpgl_ia32_intf.c
@@ -179,6 +179,8 @@ if(TVPCPUType & TVP_CPU_HAS_MMX)
 if(TVPCPUType & TVP_CPU_HAS_MMX)
 	 TVPInterpStretchCopy =  TVPInterpStretchCopy_mmx_a;
 if(TVPCPUType & TVP_CPU_HAS_MMX)
+	 TVPInterpStretchConstAlphaBlend =  TVPInterpStretchConstAlphaBlend_mmx_a;
+if(TVPCPUType & TVP_CPU_HAS_MMX)
 	 TVPInitUnivTransBlendTable =  TVPInitUnivTransBlendTable_mmx_c;
 if(TVPCPUType & TVP_CPU_HAS_MMX)
 	 TVPUnivTransBlend =  TVPUnivTransBlend_mmx_a;
