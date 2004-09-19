ba82e86b1ad1d8381863c02499c2e4843704c864
jin1016 <info@kaede-software.com>
2004-09-19 00:12:53 +0000

    追加
    git-svn-id: svn://localhost/kirikiri2/branches/layeredmovie@438 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ kirikiri2/src/core/visual/win32/krmovie/CIStream.h
@@ -0,0 +1,105 @@
+
+#ifndef __CISTREM_H__
+#define __CISTREM_H__
+
+#include <objidl.h>
+//#include <streams.h>	// 本来はあった方がいいけど、ワーニングが出るのでコメントアウト。ソースのインクルード順で回避？
+#include "asyncio.h"
+#include "asyncrdr.h"
+
+
+//---------------------------------------------------------------------------
+// CIStreamProxy : IStream to CAsyncStream proxy
+//---------------------------------------------------------------------------
+class CIStreamProxy : public CAsyncStream
+{
+public:
+    CIStreamProxy(IStream *refstream, unsigned __int64 size) :
+        m_RefStream(refstream),
+        m_Size(size)
+    {
+		m_RefStream->AddRef();
+    }
+
+	~CIStreamProxy()
+	{
+		m_RefStream->Release();
+	}
+
+	HRESULT SetPointer(LONGLONG llPos)
+    {
+		ULARGE_INTEGER result;
+		LARGE_INTEGER to;
+		to.QuadPart = llPos;
+		HRESULT hr = m_RefStream->Seek(to, STREAM_SEEK_SET, &result); 
+		if(SUCCEEDED(hr) && llPos != (LONGLONG)result.QuadPart) return E_FAIL;
+		return hr;
+    }
+    
+	HRESULT Read(PBYTE pbBuffer,
+                 DWORD dwBytesToRead,
+                 BOOL bAlign,
+                 LPDWORD pdwBytesRead)
+    {
+        CAutoLock lck(&m_csLock);
+
+		ULONG  bytesread;
+		HRESULT hr = m_RefStream->Read(pbBuffer, dwBytesToRead, &bytesread);
+		*pdwBytesRead = bytesread;
+		return hr;
+    }
+
+    LONGLONG Size(LONGLONG *pSizeAvailable)
+    {
+        *pSizeAvailable = m_Size;
+        return m_Size;
+    }
+    
+	DWORD Alignment()
+    {
+        return 1;
+    }
+    
+	void Lock()
+    {
+        m_csLock.Lock();
+    }
+    
+	void Unlock()
+    {
+        m_csLock.Unlock();
+    }
+
+private:
+    CCritSec		 m_csLock;
+	IStream	 *		 m_RefStream;
+	unsigned __int64 m_Size;
+};
+//---------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
+// CIStreamReader 
+//---------------------------------------------------------------------------
+class CIStreamReader : public CAsyncReader
+{
+public:
+    STDMETHODIMP Register()
+    {
+        return S_OK;
+    }
+    STDMETHODIMP Unregister()
+    {
+        return S_OK;
+    }
+    CIStreamReader(CIStreamProxy *pStream, CMediaType *pmt, HRESULT *phr) :
+        CAsyncReader(NAME("IStream Reader"), NULL, pStream, phr)
+    {
+        m_mt = *pmt;
+    }
+	LPCWSTR Name() { return m_pName; }
+};
+//---------------------------------------------------------------------------
+
+#endif	// __CISTREM_H__
