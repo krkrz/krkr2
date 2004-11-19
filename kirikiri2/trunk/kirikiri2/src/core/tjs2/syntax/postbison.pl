1edc7db85f8b1b3c5f1d2294f4febefec5a6a43f
w-dee <w-dee@users.noreply.github.com>
2004-11-19 14:35:34 +0000

    bison生成ファイル中のパスを修正 (これでも治ってないかも知れないが...)
    git-svn-id: svn://localhost@561 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/core/tjs2/syntax/postbison.pl
+++ kirikiri2/trunk/kirikiri2/src/core/tjs2/syntax/postbison.pl
@@ -9,11 +9,10 @@ $all = join('', @all);
 
 $all =~ s/(YYSTYPE;)/$1\nYYLEX_PROTO_DECL\n/i;
 $all =~ s/NS_TJS_START/namespace TJS \{/;
-$all =~ s/\#line (\d+) \"tjs.y\"/\#line $1 \"..\\bison\\tjs.y\"/g;
-$all =~ s/\#line (\d+) \"tjs.yy.c\"/\#line $1 \"..\\bison\\tjs.yy.cpp\"/g;
-$all =~ s/\#line (\d+) \"tjspp.y\"/\#line $1 \"..\\bison\\tjspp.y\"/g;
-$all =~ s/\#line (\d+) \"tjspp.yy.c\"/\#line $1 \"..\\bison\\tjspp.yy.cpp\"/g;
-$all =~ s/\#line (\d+) \"\/usr\/share\/bison\.simple\"/\#line $1 \"..\\bison\\bison\.simple\"/g;
+$all =~ s/\#line (\d+) \"tjs.y\"/\#line $1 \"syntax\/tjs.y\"/g;
+$all =~ s/\#line (\d+) \"tjs.yy.c\"/\#line $1 \"syntax\/tjs.yy.cpp\"/g;
+$all =~ s/\#line (\d+) \"tjspp.y\"/\#line $1 \"syntax\/tjspp.y\"/g;
+$all =~ s/\#line (\d+) \"tjspp.yy.c\"/\#line $1 \"syntax\/tjspp.yy.cpp\"/g;
 ;# $all =~ s/\"\, expecting \`\" : \" or \`\"/\"\, expecting \" : \" or \"/g;
 ;# $all =~ s/strcat\(msg\, \"\'\"\)\;//g;
 
