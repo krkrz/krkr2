076eb21a3ae667a83ac89ebe1dcb929f18de2578
w-dee <w-dee@users.noreply.github.com>
2004-12-31 15:11:50 +0000

    毎年恒例 copyright付け替え祭り'05 あけましておめでとうございます。今年もよろしくお願い致します。
    git-svn-id: svn://localhost@656 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/core/base/win32/makestub.pl
+++ kirikiri2/trunk/kirikiri2/src/core/base/win32/makestub.pl
@@ -3,7 +3,7 @@ $copyright = <<EOF;
 /*
 
 	TVP2 ( T Visual Presenter 2 )  A script authoring tool
-	Copyright (C) 2000-2004  W.Dee <dee\@kikyou.info>
+	Copyright (C) 2000-2005  W.Dee <dee\@kikyou.info>
 
 	See details of license at "license.txt"
 */
--- kirikiri2/trunk/kirikiri2/src/tools/win32/krdevui/tpc/psd/DeePSDUnit.h
+++ kirikiri2/trunk/kirikiri2/src/tools/win32/krdevui/tpc/psd/DeePSDUnit.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
 /*
 	fgconvert   (graphic converter for transparent images)
-	Copyright (C) 2000-2004  W.Dee <dee@kikyou.info>
+	Copyright (C) 2000-2005  W.Dee <dee@kikyou.info>
 
 	See details of license at "license.txt"
 */
