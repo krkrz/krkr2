86c043756178fe327b5d71beec19f81caef3d6d6
w-dee <w-dee@users.noreply.github.com>
2005-04-11 14:20:07 +0000

    XP3暗号化・復号化プラグイン です・ます調の統一、説明の追加
    git-svn-id: svn://localhost@794 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/plugins/win32/xp3filter/xp3dec/main.cpp
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/xp3filter/xp3dec/main.cpp
@@ -1,18 +1,25 @@
 //---------------------------------------------------------------------------
 /*
 	このソースでは、xp3enc.dll を使って Releaser で暗号化した XP3 ファイルを
-	扱えるようにする吉里吉里用プラグインを説明する。
+	扱えるようにする吉里吉里用プラグインを説明します。
 
-	プラグインのファイル名は、拡張子が tpm ならばなんでもかまわない。
+	プラグインのファイル名は、拡張子が tpm ならば任意の物でかまいません。
 	吉里吉里は拡張子が tpm のファイルを見つけると startup.tjs を読み込もうと
 	する前、すなわち XP3 ファイルが最初に開かれる前に読み込むので、拡張子は
-	tpm である必要がある。
+	tpm である必要があります。
 	拡張子が tpm ならばファイル名は(プラグイン間の依存性がなければ)なんでも
-	かまわないが、ここでは便宜上、xp3dec.tpm とする。
+	かまいませんが、ここでは便宜上、xp3dec.tpm とします。
 
-	xp3enc.dll と異なり、xp3dec.tpm は吉里吉里本体のプラグインとして動作する。
-	そのため、吉里吉里本体用のプラグインの作法に従って作成されなければなら
-	ないが、吉里吉里本体の各機能を利用できる。
+	xp3enc.dll と異なり、xp3dec.tpm は吉里吉里本体のプラグインとして動作しま
+	す。そのため、吉里吉里本体用のプラグインの作法に従って作成されなければな
+	らなりませんが、tp_stub.h を介して利用可能になる、吉里吉里本体の各機能を
+	利用することができます。ただし、TVPSetXP3ArchiveExtractionFilter は複数
+	スレッドから同時に呼び出される可能性がありますが、tp_stub.h を介して利用
+	可能になる関数はほとんどがメインスレッドからの呼び出しのみに対応している
+	ので注意してください。
+
+	暗号方式に関する諸注意について、 xp3enc.dll のサンプルのソース main.cpp
+	も参照してください。
 */
 
 
@@ -22,11 +29,11 @@
 //---------------------------------------------------------------------------
 #include <windows.h>
 #include "tp_stub.h"
-	// tp_stub.h にはインクルード・パスを指定しておく
+	// tp_stub.h にはインクルード・パスを指定しておきます。
 	// tp_stub.cpp および tp_stub.h のバージョンは吉里吉里本体と
-	// 同時期の物であることが好ましい
+	// 同時期の物であることが好ましいです。
 	// (もちろん本体側のインターフェースが変わらなければ、古い
-	// xp3dec.dll でも継続して利用できる)
+	// xp3dec.dll でも継続して利用できます)
 //---------------------------------------------------------------------------
 
 
@@ -36,15 +43,19 @@ void TVP_tTVPXP3ArchiveExtractionFilter_CONVENTION
 	TVPXP3ArchiveExtractionFilter(tTVPXP3ExtractionFilterInfo *info)
 {
 	// TVPXP3ArchiveExtractionFilter 関数は本体側から呼び出される
-	// コールバック関数である。
-	// 引数を一つ取り、それは tTVPXP3ExtractionFilterInfo 構造体へのポインタである。
+	// コールバック関数です。
+	// 引数を一つ取り、それは tTVPXP3ExtractionFilterInfo 構造体へのポインタ
+	// です。
 
-	// TVPXP3ArchiveExtractionFilter は、後述の
-	// V2Link 関数内で TVPSetXP3ArchiveExtractionFilter により
-	// 設定される。
+	// TVPXP3ArchiveExtractionFilter は、後述の V2Link 関数内で
+	// TVPSetXP3ArchiveExtractionFilter により設定されます。
 
 	// ここでは単純に、xp3enc.dll のサンプルで作成された XP3 アーカイブを
-	// 復号すべく、データをすべて FileHash の最下位バイトで XOR することにする。
+	// 復号すべく、データをすべて FileHash の最下位バイトで XOR
+	// することにします。
+
+	// この関数は複数のスレッドから同時に呼び出される可能性があるので
+	// 注意してください。
 
 	/*
 		tTVPXP3ExtractionFilterInfo のメンバは以下の通り
@@ -86,7 +97,7 @@ int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason,
 //---------------------------------------------------------------------------
 extern "C" HRESULT _stdcall V2Link(iTVPFunctionExporter *exporter)
 {
-	// V2Link は、吉里吉里本体とリンクされるときに呼び出される
+	// V2Link は、吉里吉里本体とリンクされるときに呼び出される関数です
 	
 	// スタブの初期化(必ず記述する)
 	TVPInitImportStub(exporter);
@@ -100,7 +111,7 @@ extern "C" HRESULT _stdcall V2Link(iTVPFunctionExporter *exporter)
 //---------------------------------------------------------------------------
 extern "C" HRESULT _stdcall V2Unlink()
 {
-	// V2Unlink は、吉里吉里本体から切り離されるときに呼び出される
+	// V2Unlink は、吉里吉里本体から切り離されるときに呼び出される関数です
 
 	// 一応 TVPSetXP3ArchiveExtractionFilter に NULL を渡し、
 	// コールバック関数の登録を解除する
