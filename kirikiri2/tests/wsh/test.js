683263fa0a815addd109462edac97dc98dbbc34b
w-dee <w-dee@users.noreply.github.com>
2005-07-22 05:32:54 +0000

    ごうさんの Windows Scripting Host プラグイン 用のテストスクリプト
    git-svn-id: svn://localhost/kirikiri2/trunk@1014 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ kirikiri2/tests/wsh/test.js
@@ -0,0 +1,11 @@
+//
+// JScript による記述
+// って TJS と全然かわらんけど^^;
+//
+var win = new Window();
+win.setInnerSize(400,400);
+var base = new Layer(win, null);
+base.setSize(400,400);
+base.fillRect(0,0,400,400,0x00ffff);
+base.visible = true;
+win.visible = true;
