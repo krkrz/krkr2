bc7dff9435a9d7e220ca6dae88f9cc9b33e53f4a
wtnbgo <go@dsl.gr.jp>
2005-09-17 18:23:47 +0000

    レイヤ拡張プラグイン　初期コード　新規追加
    git-svn-id: svn://localhost/kirikiri2/trunk@1115 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ kirikiri2/src/plugins/win32/layerEx/LayerExImage.h
@@ -0,0 +1,42 @@
+#ifndef __LayerExImage__
+#define __LayerExImage__
+
+#include "LayerExBase.h"
+
+/**
+ * レイヤ拡張 イメージ操作用ネイティブインスタンス
+ */
+class NI_LayerExImage : public NI_LayerExBase
+{
+public:
+	NI_LayerExImage(iTJSDispatch2 *layerobj) : NI_LayerExBase(layerobj) {};
+	
+	/**
+	 * ルックアップテーブル反映
+	 * @param pLut lookup table
+	 */
+	void lut(BYTE* pLut);
+	
+	/**
+	 * 明度とコントラスト
+	 * @param brightness 明度 -255 ～ 255, 負数の場合は暗くなる
+	 * @param contrast コントラスト -100 ～100, 0 の場合変化しない
+	 */
+	void light(int brightness, int contrast);
+
+	/**
+	 * 色相と彩度
+	 * @param hue 色相
+	 * @param sat 彩度
+	 * @param blend ブレンド 0 (効果なし) ～ 1 (full effect)
+	 */
+	void colorize(int hue, int sat, double blend);
+
+	/**
+	 * ノイズ追加
+	 * @param level ノイズレベル 0 (no noise) ～ 255 (lot of noise).
+	 */
+	void noise(int level);
+};
+
+#endif
