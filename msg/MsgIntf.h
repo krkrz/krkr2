48da2042f44f87f804363a238784166841ea6e4c
w-dee <w-dee@users.noreply.github.com>
2005-12-31 23:54:31 +0000

    毎年恒例 Copyright 付け替え祭り '06 (吉里吉里2 trunk)
    あけましておめでとうございます。今年もよろしくお願いいたします。
    
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src/core@1258 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- msg/MsgIntf.h
+++ msg/MsgIntf.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
 /*
 	TVP2 ( T Visual Presenter 2 )  A script authoring tool
-	Copyright (C) 2000-2005  W.Dee <dee@kikyou.info> and contributors
+	Copyright (C) 2000-2006 W.Dee <dee@kikyou.info> and contributors
 
 	See details of license at "license.txt"
 */
@@ -32,7 +32,7 @@ TVP_MSG_DECL_CONST(TVPAboutString,
 TJS_W("吉里吉里[きりきり] 2 実行コア version %1 ( TJS version %2 )\n")
 TJS_W("Compiled on ") __DATE__ TJS_W(" ") __TIME__ TJS_W("\n")
 TVP_SVN_REVISION TJS_W("\n")
-TJS_W("Copyright (C) 1997-2005 W.Dee and contributors All rights reserved.\n")
+TJS_W("Copyright (C) 1997-2006 W.Dee and contributors All rights reserved.\n")
 TJS_W("Contributors in alphabetical order:\n")
 TJS_W("  Go Watanabe, Kenjo, Kiyobee, Kouhei Yanagita, MIK, Takenori Imoto, yun\n")
 TJS_W("吉里吉里実行コアの使用/配布/改変は、\n")
@@ -83,7 +83,7 @@ TVP_MSG_DECL_CONST(TVPVersionInformation,
 	
 TJS_W("吉里吉里[きりきり] 2 実行コア/%1 ")
 TJS_W("(SVN revision:") TVP_SVN_REVISION_NUMBER TJS_W("; Compiled on ") __DATE__ TJS_W(" ") __TIME__ TJS_W(") TJS2/%2 ")
-TJS_W("Copyright (C) 1997-2005 W.Dee and contributors All rights reserved."));
+TJS_W("Copyright (C) 1997-2006 W.Dee and contributors All rights reserved."));
 
 TVP_MSG_DECL_CONST(TVPVersionInformation2,
 	TJS_W("バージョン情報の詳細は Ctrl + F12 で閲覧できます"));
