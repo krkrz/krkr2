c91deb5c96c7fc366a9c8bfaecbd72b79a2a067a
wtnbgo <go@dsl.gr.jp>
2006-07-18 12:35:25 +0000

    オブジェクト呼び出し部修正
    git-svn-id: svn://localhost@1700 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/plugins/win32/squirrel/sqwrapper.cpp
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/squirrel/sqwrapper.cpp
@@ -2,11 +2,17 @@
 #include "tp_stub.h"
 #include "sqplus.h"
 
+void store(HSQUIRRELVM v, iTJSDispatch2 *dispatch);
 void store(HSQUIRRELVM v, tTJSVariant &variant);
 void store(tTJSVariant &result, SquirrelObject &variant);
 void store(tTJSVariant &result, StackHandler &stack, int idx=1);
 void store(tTJSVariant &result, HSQUIRRELVM v, int idx=-1);
 
+
+
+/**
+ * 開放処理
+ */
 static SQInteger
 tjsDispatchRelease(SQUserPointer up, SQInteger size)
 {
@@ -15,6 +21,9 @@ tjsDispatchRelease(SQUserPointer up, SQInteger size)
 	return 1;
 }
 
+/**
+ * プロパティの設定
+ */
 static SQInteger
 set(HSQUIRRELVM v)
 {
@@ -29,7 +38,9 @@ set(HSQUIRRELVM v)
 	return 0;
 }
 
-
+/**
+ * プロパティの取得
+ */
 static SQInteger
 get(HSQUIRRELVM v)
 {
@@ -46,7 +57,81 @@ get(HSQUIRRELVM v)
 	return 0;
 }
 
-static void
+/**
+ * プロパティの削除
+ */
+static SQInteger
+delslot(HSQUIRRELVM v)
+{
+	StackHandler stack(v);
+	SQUserPointer up = stack.GetUserData(1);
+	if (up) {
+		iTJSDispatch2 *dispatch	= *((iTJSDispatch2**)up);
+		dispatch->DeleteMember(0, stack.GetString(2), NULL, dispatch);
+	}
+	return 0;
+}
+
+/**
+ * メソッドまたはコンストラクタの呼び出し
+ */
+static SQInteger
+call(HSQUIRRELVM v)
+{
+	// param1 オブジェクト
+	// param2 オリジナルオブジェクト
+	// param3 ～ 本来の引数ぽ
+
+	StackHandler stack(v);
+	SQUserPointer up = stack.GetUserData(1);
+	int ret = 0;
+	if (up) {
+		iTJSDispatch2 *dispatch	= *((iTJSDispatch2**)up);
+
+		// this を取得
+		iTJSDispatch2 *thisobj = NULL;
+		up = stack.GetUserData(2);
+		if (up) {
+			thisobj = *((iTJSDispatch2**)up);
+		}
+		
+		int argc = stack.GetParamCount() - 2;
+		
+		// 引数変換
+		tTJSVariant **args = new tTJSVariant*[argc];
+		for (int i=0;i<argc;i++) {
+			args[i] = new tTJSVariant();
+			store(*args[i], stack, i + 3);
+		}
+
+		if (dispatch->IsInstanceOf(0, NULL, NULL, L"Class", dispatch) == TJS_S_TRUE) {
+			// クラスオブジェクトの場合コンストラクタとみなす
+			iTJSDispatch2 *instance = NULL;
+			if (SUCCEEDED(dispatch->CreateNew(0, NULL, NULL, &instance, argc, args, thisobj))) {
+				store(v, instance);
+				ret = 1;
+			}
+		} else {
+			// メソッド呼び出し
+			tTJSVariant result;
+			if (SUCCEEDED(dispatch->FuncCall(0, NULL, NULL, &result, argc, args, thisobj))) {
+				if (result.Type() != tvtVoid) {
+					store(v, result);
+					ret = 1;
+				}
+			}
+		}
+			
+		// 引数破棄
+		for (int i=0;i<argc;i++) {
+			delete args[i];
+		}
+		delete[] args;
+	}
+	return ret;
+}
+
+void
 store(HSQUIRRELVM v, iTJSDispatch2 *dispatch)
 {
 	if (dispatch) {
@@ -68,13 +153,17 @@ store(HSQUIRRELVM v, iTJSDispatch2 *dispatch)
 		sq_newclosure(v, get, 0);
 		sq_createslot(v, -3);
 
-		sq_pushstring(v, L"_get", -1);
-		sq_newclosure(v, get, 0);
+		sq_pushstring(v, L"_newslot", -1);
+		sq_newclosure(v, set, 0);
+		sq_createslot(v, -3);
+		
+		sq_pushstring(v, L"_delslot", -1);
+		sq_newclosure(v, delslot, 0);
 		sq_createslot(v, -3);
 
-		
-
-
+		sq_pushstring(v, L"_call", -1);
+		sq_newclosure(v, call, 0);
+		sq_createslot(v, -3);
 		
 		sq_setdelegate(v, -2);
 
@@ -109,41 +198,6 @@ store(HSQUIRRELVM v, tTJSVariant &variant)
 }
 
 void
-store(tTJSVariant &result, SquirrelObject &variant)
-{
-	result.Clear();
-	switch (variant.GetType()) {
-	case OT_NULL:
-		break;
-	case OT_INTEGER:
-		result = variant.ToInteger();
-		break;
-	case OT_FLOAT:
-		result = variant.ToFloat();
-		break;
-	case OT_BOOL:
-		result = variant.ToBool() ? "1" : "0";
-		break;
-	case OT_STRING:
-		result = variant.ToString();
-		break;
-	case OT_TABLE:
-	case OT_ARRAY:
-	case OT_USERDATA:
-	case OT_CLOSURE:
-	case OT_NATIVECLOSURE:
-	case OT_GENERATOR:
-	case OT_USERPOINTER:
-	case OT_THREAD:
-	case OT_CLASS:
-	case OT_INSTANCE:
-	case OT_WEAKREF:
-		// ラッピングが必要!
-		break;
-	}
-}
-
-void
 store(tTJSVariant &result, StackHandler &stack, int idx)
 {
 	result.Clear();
@@ -162,9 +216,16 @@ store(tTJSVariant &result, StackHandler &stack, int idx)
 	case OT_STRING:
 		result = stack.GetString(idx);
 		break;
+	case OT_USERDATA:
+		{
+			SQUserPointer up = stack.GetUserData(idx);
+			if (up) {
+				result = *((iTJSDispatch2**)up);
+			}
+		}
+		break;
 	case OT_TABLE:
 	case OT_ARRAY:
-	case OT_USERDATA:
 	case OT_CLOSURE:
 	case OT_NATIVECLOSURE:
 	case OT_GENERATOR:
@@ -182,6 +243,14 @@ void
 store(tTJSVariant &result, HSQUIRRELVM v, int idx)
 {
 	store(result, StackHandler(v), idx);
+}
+
+void
+store(tTJSVariant &result, SquirrelObject &variant)
+{
+	StackHandler stack(SquirrelVM::GetVMPtr());
+	stack.Return(variant);
+	store(result, stack);
 }
 
 // -------------------------------------------------------
