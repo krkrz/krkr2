2a057a32a4b609b9ab88d29bf9aa242450eac8b0
sugi <sugi@users.noreply.github.com>
2006-10-17 15:46:08 +0000

    キーワード検索の高速化。
    git-svn-id: svn://localhost@1981 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/docs/tjs2/j/contents/keywordgrep.js
+++ kirikiri2/trunk/kirikiri2/docs/tjs2/j/contents/keywordgrep.js
@@ -1,49 +1,57 @@
-var spans = document.getElementsByTagName('span');
-var lastValue = '';
+function Searcher() {
+    var spans = document.getElementsByTagName('span');
+    var styles = [];
+    var text1 = [];
+    var text2 = [];
+    var spans_length = spans.length;
 
-function grep(str) {
-  if (str == lastValue) {
-      return;
-  } else {
-      lastValue = str;
-  }
-
-  var regs = [];
-  var words = str.split(' ');
-  for (var i = 0; i < words.length; ++i) {
-    if (words[i] != '') {
-      regs.push(new RegExp(words[i].replace(/(\W)/g, "\\$1"), 'i'));
-    }
-  }
-
-  var n = spans.length;
-  for (var i = 0; i < n; ++i) {
-    var span = spans[i];
-    var text1 = span.parentNode.childNodes[0].nodeValue;
-    var text2 = span.childNodes[0].nodeValue;
-
-    var matched = true;
-    for (var j = 0; j < regs.length; ++j) {
-      if (!regs[j].test(text1) && !regs[j].test(text2)) {
-        matched = false;
-        break;
-      }
+    for (var i = 0; i < spans_length; ++i) {
+        var span = spans[i];
+        styles.push(span.parentNode.parentNode.parentNode.parentNode.style);
+        text1.push(span.parentNode.childNodes[0].nodeValue);
+        text2.push(span.childNodes[0].nodeValue);
     }
 
-    if (matched) {
-      span.parentNode.parentNode.parentNode.parentNode.style.display = '';
-    } else {
-      span.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+    var lastValue = '';
+
+    this.grep = function (str) {
+        if (str == lastValue) {
+            return;
+        } else {
+            lastValue = str;
+        }
+
+        var regs = [];
+        var words = str.split(' ');
+        for (var i = 0; i < words.length; ++i) {
+            if (words[i] != '') {
+                regs.push(new RegExp(words[i].replace(/(\W)/g, "\\$1"), 'i'));
+            }
+        }
+
+        for (var i = 0; i < spans_length; ++i) {
+            var span = spans[i];
+            var matched = true;
+            for (var j = 0; j < regs.length; ++j) {
+                if (!regs[j].test(text1[i]) && !regs[j].test(text2[i])) {
+                    matched = false;
+                    break;
+                }
+            }
+
+            styles[i].display = matched ? '' : 'none';
+        }
     }
-  }
+}
+
+var searcher;
+function init() {
+    searcher = new Searcher();
+    setInterval(onTimer, 400);
 }
 
 function onTimer() {
-    grep(document.getElementById('grepword').value);
-}
-
-function init() {
-    setInterval(onTimer, 500);
+    searcher.grep(document.getElementById('grepword').value);
 }
 
 window.onload = init;
