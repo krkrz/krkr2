d4fa66764d1778d4d4859a4c521d47553e3a11e1
w-dee <w-dee@users.noreply.github.com>
2006-10-20 14:57:37 +0000

    Subversion の作業コピーの形式が変わったらしく、作業コピーからリビジョンを取得するのが従来の方法ではできなくなった。
    さほど重要ではないのでリビジョンの埋め込みは当面辞めることにする。
    
    git-svn-id: svn://localhost/kirikiri2/trunk@2001 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/src/core/environ/win32/_release.bat
+++ kirikiri2/src/core/environ/win32/_release.bat
@@ -21,10 +21,6 @@ copy tvpwin32.bpr tvpwin32.~bpr /y
 copy tvpwin32.res tvpwin32.~res /y
 perl basefix.pl
 
-cd %TOP_DIR%\msg
-copy svn_revision.h svn_revision.~h /y
-perl update_svn_revision.pl
-
 cd %TOP_DIR%\environ\win32
 "c:\Program Files\Borland\CBuilder5\Bin\bcb" tvpwin32.bpr -b -omakeout.txt
 
@@ -35,7 +31,4 @@ copy /b tvpwin32.exe+"%TOP_DIR%\environ\win32\optionarea.bin" krkr.eXe /y
 cd %TOP_DIR%\environ\win32
 copy tvpwin32.~bpr tvpwin32.bpr /y
 copy tvpwin32.~res tvpwin32.res /y
-
-cd %TOP_DIR%\msg
-copy svn_revision.~h svn_revision.h /y
 
