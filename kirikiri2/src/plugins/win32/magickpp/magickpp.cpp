78052b1964cfee7b6ab7df8caaf69a965678166d
miahmie <miahmie@users.noreply.github.com>
2007-02-18 20:09:53 +0000

    ImageMagick/Magick++ Plugins pre-alpha version. (require ncbind)
    git-svn-id: svn://localhost/kirikiri2/trunk@2381 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ kirikiri2/src/plugins/win32/magickpp/magickpp.cpp
@@ -0,0 +1,75 @@
+#include <list>
+#include <string>
+
+#include "magickpp.hpp"
+
+struct MagickPP {
+	template <typename T> struct Container { typedef std::list<T> Type; };
+
+	typedef char const* NameT;
+
+	typedef Magick::CoderInfo CoderInfoT;
+	typedef Magick::Image     ImageT;
+
+	typedef Container<CoderInfoT>::Type CoderListT;
+	typedef Container<ImageT>::Type     ImageListT;
+
+	typedef ncbInstanceAdaptor<CoderInfoT>       CoderAdaptorT;
+	typedef ncbInstanceAdaptor<ImageT>           ImageAdaptorT;
+
+
+	// イメージ(群)読み込み
+	static iTJSDispatch2* readImages(StringT const &spec) {
+
+		ImageListT lst;
+		Magick::readImages(&lst, spec);
+
+		// Array オブジェクトを作成
+		iTJSDispatch2 *array = TJSCreateArrayObject();
+		tjs_uint32 hint = 0;
+
+		for (ImageListT::iterator it = lst.begin(); it != lst.end(); it++) {
+			ImageT *img = new ImageT(*it);
+			iTJSDispatch2 *adp = ImageAdaptorT::CreateAdaptor(img, false);
+			tTJSVariant var(adp), *param = &var;
+			adp->Release();
+			array->FuncCall(0, TJS_W("add"), &hint, 0, 1, &param, array);
+		}
+		return array;
+	}
+
+	// Corder一覧取得
+	static iTJSDispatch2* supports() {
+
+		CoderListT cl; 
+		Magick::coderInfoList(&cl,                   // Reference to output list 
+							  CoderInfoT::AnyMatch,  // readable formats
+							  CoderInfoT::AnyMatch,  // writable formats
+							  CoderInfoT::AnyMatch); // multi-frame support
+
+		// Array オブジェクトを作成
+		iTJSDispatch2 *array = TJSCreateArrayObject();
+		tjs_uint32 hint = 0;
+
+		ttstr exp(TJS_W("new MagickPP_CoderInfo('"));
+		for (CoderListT::const_iterator it = cl.begin(); it != cl.end(); it++) {
+			tTJSVariant var, *param = &var;
+			TVPExecuteExpression(exp + ttstr(it->name().c_str()) + TJS_W("')"), &var);
+			array->FuncCall(0, TJS_W("add"), &hint, 0, 1, &param, array);
+		}
+
+		return array;
+	}
+
+	// ImageMagick バージョン取得
+	static NameT version() { return MagickVersion; }
+
+
+};
+
+
+NCB_REGISTER_CLASS(MagickPP) {
+	PROP_RO(version);
+	PROP_RO(supports);
+}
+
