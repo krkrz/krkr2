8ac26a0dcce61beef1369fc8c4795a1cc08619ec
wtnbgo <go@dsl.gr.jp>
2007-02-24 02:26:31 +0000

    layerExCairo を gameswf から分離維持
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src@2401 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ plugins/win32/layerExCairo/layerExCairo.hpp
@@ -0,0 +1,32 @@
+#ifndef _layExCairo_hpp_
+#define _layExCairo_hpp_
+
+#include <windows.h>
+#include "layerExBase.hpp"
+#include "cairo.h"
+
+/*
+ * Cairo 描画用レイヤ
+ */
+struct layerExCairo : public layerExBase
+{
+protected:
+	GeometryT width;
+	GeometryT height;
+	BufferT buffer;
+	PitchT pitch;
+	cairo_surface_t *surface;
+	cairo_t * cairo;
+
+public:
+	layerExCairo(DispatchT obj);
+	~layerExCairo();
+	
+public:
+	/**
+	 * レイヤ情報リセット処理
+	 */
+	virtual void reset();
+};
+
+#endif
