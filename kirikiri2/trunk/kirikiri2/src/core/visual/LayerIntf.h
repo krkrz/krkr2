fd73d40ab292cc94fe9409813f07fe4b3ddab1a4
w-dee <w-dee@users.noreply.github.com>
2007-03-18 12:14:51 +0000

    LayerIntf.cpp / LayerIntf.h を LayerManager.cpp と LayerManager.h に分割
    git-svn-id: svn://localhost@2485 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/core/visual/LayerIntf.h
+++ kirikiri2/trunk/kirikiri2/src/core/visual/LayerIntf.h
@@ -133,6 +133,18 @@ enum tTVPHitType {htMask, htProvince};
 /*]*/
 
 
+//---------------------------------------------------------------------------
+// update optimization options
+//---------------------------------------------------------------------------
+#define TVP_UPDATE_UNITE_LIMIT 300
+#define TVP_CACHE_UNITE_LIMIT 60
+#define TVP_EXPOSED_UNITE_LIMIT 30
+#define TVP_DIRECT_UNITE_LIMIT 10
+//---------------------------------------------------------------------------
+
+
+
+
 
 //---------------------------------------------------------------------------
 // TVPToActualColor :  convert color identifier to actual color
@@ -975,199 +987,6 @@ private:
 };
 //---------------------------------------------------------------------------
 
-
-
-//---------------------------------------------------------------------------
-// tTVPLayerManager
-//---------------------------------------------------------------------------
-// layer mamager which is created in window's context
-//---------------------------------------------------------------------------
-class tTVPLayerManager
-{
-	tjs_int RefCount; //!< reference count
-	tTJSNI_BaseWindow * Window;
-
-	tTJSNI_BaseLayer * CaptureOwner;
-	tTJSNI_BaseLayer * LastMouseMoveSent;
-
-	std::vector<tTJSNI_BaseLayer *> ModalLayerVector;
-		// pointer to modal layer vector
-	tTJSNI_BaseLayer * FocusedLayer; // pointer to current focused layer
-	tTJSNI_BaseLayer * Primary; // primary layer
-	bool OverallOrderIndexValid;
-	std::vector<tTJSNI_BaseLayer*> AllNodes;
-		// hold overall nodes;
-		// use GetAllNodes to retrieve the newest information of this
-	tTVPComplexRect UpdateRegion; // window update region
-
-	bool FocusChangeLock;
-
-	bool VisualStateChanged;
-		// flag for visual
-		// state changing ( see tTJSNI_BaseLaye::NotifyChildrenVisualStateChanged)
-
-	tjs_int LastMouseMoveX;
-	tjs_int LastMouseMoveY;
-
-	bool ReleaseCaptureCalled;
-
-	bool InNotifyingHintOrCursorChange;
-
-public:
-	tTVPLayerManager(tTJSNI_BaseWindow *window);
-private:
-	virtual ~tTVPLayerManager();
-public:
-	virtual void TJS_INTF_METHOD AddRef();
-	virtual void TJS_INTF_METHOD Release();
-
-public:
-	void AttachPrimary(tTJSNI_BaseLayer *pri); // attach primary layer to the manager
-	void DetachPrimary(); // detach primary layer from the manager
-
-	tTJSNI_BaseLayer * GetPrimaryLayer() const { return Primary; }
-	bool IsPrimaryLayerAttached() const { return Primary != NULL; } 
-
-	bool GetPrimaryLayerSize(tjs_int &w, tjs_int &h) const;
-
-	void NotifyPart(tTJSNI_BaseLayer *lay); // notifies layer parting from its parent
-
-	tTVPComplexRect & GetUpdateRegionForCompletion() { return UpdateRegion; }
-
-private:
-
-	void _RecreateOverallOrderIndex(tjs_uint& index,
-		std::vector<tTJSNI_BaseLayer*>& nodes);
-
-public:
-	void InvalidateOverallIndex();
-	void RecreateOverallOrderIndex();
-
-	std::vector<tTJSNI_BaseLayer*> & GetAllNodes();
-
-	void NotifyVisualStateChanged() { VisualStateChanged = true; }
-	bool GetVisualStateChanged() { return VisualStateChanged; }
-	void QueryUpdateExcludeRect();
-
-
-public:
-
-	void NotifyMouseCursorChange(tTJSNI_BaseLayer *layer, tjs_int cursor);
-	void SetMouseCursor(tjs_int cursor);
-
-	void GetCursorPos(tjs_int &x, tjs_int &y);
-		// get cursor position in primary coordinates
-	void SetCursorPos(tjs_int x, tjs_int y);
-		// set cursor position in primary coordinates
-
-	void NotifyHintChange(tTJSNI_BaseLayer *layer, const ttstr & hint);
-	void SetHint(const ttstr &hint);
-		// set layer hint to current window
-	void NotifyLayerResize();  // layer -> window
-	void NotifyWindowInvalidation(); // layer -> window
-
-public:
-	tTJSNI_BaseWindow * GetWindow() const { return Window; }
-	void SetWindow(tTJSNI_BaseWindow *window);
-	void NotifyResizeFromWindow(tjs_uint w, tjs_uint h); // window -> layer
-	void NotifyInvalidationFromWindow(const tTVPRect &r); // window -> layer
-
-	void NotifyClick(tjs_int x, tjs_int y) { PrimaryClick(x, y); }
-	void NotifyDoubleClick(tjs_int x, tjs_int y) { PrimaryDoubleClick(x, y); }
-	void NotifyMouseDown(tjs_int x, tjs_int y, tTVPMouseButton mb, tjs_uint32 flags)
-		{ PrimaryMouseDown(x, y, mb, flags); }
-	void NotifyMouseUp(tjs_int x, tjs_int y, tTVPMouseButton mb, tjs_uint32 flags)
-		{ PrimaryMouseUp(x, y, mb, flags); }
-	void NotifyMouseMove(tjs_int x, tjs_int y, tjs_uint32 flags)
-		{ PrimaryMouseMove(x, y, flags); }
-	void NotifyMouseOutOfWindow()
-		{ MouseOutOfWindow(); }
-	void NotifyKeyDown(tjs_uint key, tjs_uint32 shift)
-		{ PrimaryKeyDown(key, shift); }
-	void NotifyKeyUp(tjs_uint key, tjs_uint32 shift)
-		{ PrimaryKeyUp(key, shift); }
-	void NotifyKeyPress(tjs_char key)
-		{ PrimaryKeyPress(key); }
-	void NotifyMouseWheel(tjs_uint32 shift, tjs_int delta, tjs_int x, tjs_int y)
-		{ PrimaryMouseWheel(shift, delta, x, y); }
-
-	tTJSNI_BaseLayer * GetMostFrontChildAt(tjs_int x, tjs_int y,
-		tTJSNI_BaseLayer *except = NULL, bool get_disabled = false);
-
-	void PrimaryClick(tjs_int x, tjs_int y);
-	void PrimaryDoubleClick(tjs_int x, tjs_int y);
-
-	void PrimaryMouseDown(tjs_int x, tjs_int y, tTVPMouseButton mb, tjs_uint32 flags);
-	void PrimaryMouseUp(tjs_int x, tjs_int y, tTVPMouseButton mb, tjs_uint32 flags);
-
-	void PrimaryMouseMove(tjs_int x, tjs_int y, tjs_uint32 flags);
-	void ForceMouseLeave();
-	void ForceMouseRecheck();
-	void MouseOutOfWindow();
-	void LeaveMouseFromTree(tTJSNI_BaseLayer *root); // force to leave mouse
-
-	void ReleaseCapture();
-	void ReleaseCaptureFromTree(tTJSNI_BaseLayer * layer);
-
-	bool BlurTree(tTJSNI_BaseLayer *root); // remove focus from "root"
-	tTJSNI_BaseLayer *SearchFirstFocusable(bool ignore_chain_focusable = true); // search first focusable layer
-
-
-	tTJSNI_BaseLayer *GetFocusedLayer() const { return FocusedLayer; }
-	void CheckTreeFocusableState(tTJSNI_BaseLayer *root);
-		// check newly added tree's focusable state
-	bool SetFocusTo(tTJSNI_BaseLayer *layer, bool direction = true);
-		// set focus to layer
-	tTJSNI_BaseLayer *FocusPrev(); // focus to previous layer
-	tTJSNI_BaseLayer *FocusNext(); // focus to next layer
-	void ReleaseAllModalLayer(); // release all modal layer on invalidation
-	void SetModeTo(tTJSNI_BaseLayer *layer); // set mode to layer
-	void RemoveModeFrom(tTJSNI_BaseLayer *layer); // remove mode from layer
-	void RemoveTreeModalState(tTJSNI_BaseLayer *root);
-		// remove modal state from given tree
-	tTJSNI_BaseLayer *GetCurrentModalLayer() const;
-
-private:
-	bool SearchAttentionPoint(tTJSNI_BaseLayer *target, tjs_int &x, tjs_int &y);
-	void SetAttentionPointOf(tTJSNI_BaseLayer *layer);
-	void DisableAttentionPoint();
-
-public:
-	void NotifyAttentionStateChanged(tTJSNI_BaseLayer *from);
-
-private:
-	void SetImeModeOf(tTJSNI_BaseLayer *layer);
-	void ResetImeMode();
-
-public:
-	void NotifyImeModeChanged(tTJSNI_BaseLayer *from);
-
-private:
-	tjs_int EnabledWorkRefCount;
-public:
-	void SaveEnabledWork();
-	void NotifyNodeEnabledState();
-	void PrimaryKeyDown(tjs_uint key, tjs_uint32 shift);
-	void PrimaryKeyUp(tjs_uint key, tjs_uint32 shift);
-	void PrimaryKeyPress(tjs_char key);
-	void PrimaryMouseWheel(tjs_uint32 shift, tjs_int delta, tjs_int x, tjs_int y);
-
-	void AddUpdateRegion(const tTVPComplexRect &rects);
-	void AddUpdateRegion(const tTVPRect & rect);
-	void PrimaryUpdateByWindow(const tTVPRect &rect);
-	void UpdateToWindow();
-	void NotifyUpdateRegionFixed();
-
-public:
-	void TimerBeat();
-		// To re-check current layer under current mouse position
-		// and update hint, cursor type and process layer enter/leave.
-		// This can be reasonably slow, about 1 sec interval.
-
-public:
-	void DumpPrimaryStructure();
-};
-//---------------------------------------------------------------------------
 
 
 #include "LayerImpl.h"
