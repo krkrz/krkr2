304105f98c04ea460bbad6c0c33fd28c72cb843d
wtnbgo <go@dsl.gr.jp>
2007-04-20 21:29:34 +0000

    複数の primaryLayer に対応する処理
    git-svn-id: svn://localhost@2589 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/plugins/win32/drawdeviceIrrlicht/IrrlichtDrawDevice.h
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/drawdeviceIrrlicht/IrrlichtDrawDevice.h
@@ -16,28 +16,54 @@ using namespace std;
 
 #include "BasicDrawDevice.h"
 
+
+/**
+ * レイヤマネージャ用付随情報
+ */
+class LayerManagerInfo {
+	
+public:
+	// コンストラクタ
+	LayerManagerInfo(ITexture *texture);
+	virtual ~LayerManagerInfo();
+	
+private:
+	// コピー処理用一時変数
+	unsigned char *destBuffer;
+	int destWidth;
+	int destHeight;
+	int destPitch;
+	
+public:
+	// 割り当てテクスチャ
+	ITexture *texture;
+
+	void lock();
+	void copy(tjs_int x, tjs_int y, const void * bits, const BITMAPINFO * bitmapinfo,
+			  const tTVPRect &cliprect, tTVPLayerType type, tjs_int opacity);
+	void unlock();
+};
+
 /**
  * Irrlicht ベースの DrawDevice
  */
-class tTVPIrrlichtDrawDevice : public tTVPDrawDevice, public tTVPContinuousEventCallbackIntf
+class tTVPIrrlichtDrawDevice : public tTVPDrawDevice, public tTVPContinuousEventCallbackIntf, public IEventReceiver
 {
 	typedef tTVPDrawDevice inherited;
 
 	/// デバイス
 	IrrlichtDevice *device;
-	/// ドライバ
-	video::IVideoDriver* driver;
-	/// シーンマネージャ
-	ISceneManager* smgr;
 
-	/// レイヤ描画先テクスチャ
-	ITexture *texture;
-	
 public:
 	tTVPIrrlichtDrawDevice(); //!< コンストラクタ
 private:
 	virtual ~tTVPIrrlichtDrawDevice(); //!< デストラクタ
 
+	// テクスチャの割り当て
+	void allocInfo(iTVPLayerManager * manager);
+	// テクスチャの解放
+	void freeInfo(iTVPLayerManager * manager);
+	
 	IrrlichtDevice *create(HWND hwnd, video::E_DRIVER_TYPE type);
 	void attach(HWND hwnd);
 	void detach();
@@ -45,12 +71,10 @@ private:
 	int width;
 	int height;
 
-	unsigned char *destBuffer;
-	int destWidth;
-	int destHeight;
-	int destPitch;
-
 public:
+	//---- LayerManager の管理関連
+	virtual void TJS_INTF_METHOD AddLayerManager(iTVPLayerManager * manager);
+	virtual void TJS_INTF_METHOD RemoveLayerManager(iTVPLayerManager * manager);
 
 	//---- 描画位置・サイズ関連
 	virtual void TJS_INTF_METHOD SetTargetWindow(HWND wnd);
@@ -67,6 +91,11 @@ public:
 	// テスト用処理
 	void init();
 
+
+	// ------------------------------------------------------------
+	// 吉里吉里からの呼び出し制御用
+	// ------------------------------------------------------------
+	
 public:
 	/**
 	 * Irrlicht 呼び出し処理開始
@@ -84,6 +113,12 @@ public:
 	 * 塗り直し処理
 	 */
 	virtual void TJS_INTF_METHOD OnContinuousCallback(tjs_uint64 tick);
+
+	// ------------------------------------------------------------
+	// イベント処理用
+	// ------------------------------------------------------------
+
+	virtual bool OnEvent(SEvent event);
 };
 
 #endif
