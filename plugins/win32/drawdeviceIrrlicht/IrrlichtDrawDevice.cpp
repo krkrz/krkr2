0852a4874e7cdee6ac02064a2ff87afbcaea7854
wtnbgo <go@dsl.gr.jp>
2007-04-20 21:29:34 +0000

    複数の primaryLayer に対応する処理
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src@2589 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- plugins/win32/drawdeviceIrrlicht/IrrlichtDrawDevice.cpp
+++ plugins/win32/drawdeviceIrrlicht/IrrlichtDrawDevice.cpp
@@ -5,133 +5,46 @@
 /**
  * コンストラクタ
  */
-tTVPIrrlichtDrawDevice::tTVPIrrlichtDrawDevice()
+LayerManagerInfo::LayerManagerInfo(ITexture *texture) : texture(texture)
 {
-	device = NULL;
-	driver = NULL;
-	smgr   = NULL;
-	texture  = NULL;
 	destBuffer = NULL;
-}
+};
 
 /**
  * デストラクタ
  */
-tTVPIrrlichtDrawDevice::~tTVPIrrlichtDrawDevice()
+LayerManagerInfo::~LayerManagerInfo()
 {
-	detach();
 }
 
-/**
- * デバイスの生成
- * @param hwnd ハンドル
- * @param ドライバの種類
- */
-IrrlichtDevice *
-tTVPIrrlichtDrawDevice::create(HWND hwnd, video::E_DRIVER_TYPE type)
-{
-	SIrrlichtCreationParameters params;
-	params.WindowId     = reinterpret_cast<s32>(hwnd);
-	params.DriverType    = type;
-	params.WindowSize    = core::dimension2d<s32>(width, height);
-	params.Bits          = 32;
-	params.Stencilbuffer = true;
-	params.Vsync = true;
-	return createDeviceEx(params);
-}
 
 /**
- * ウインドウの再設定
- * @param hwnd ハンドル
+ * テクスチャをロックして描画領域情報を取得する
  */
 void
-tTVPIrrlichtDrawDevice::attach(HWND hwnd)
+LayerManagerInfo::lock()
 {
-	// ウインドウサイズの取得
-	RECT rect;
-	GetClientRect(hwnd, &rect);
-	width  = rect.right - rect.left;
-	height = rect.bottom - rect.top;
-
-	device = create(hwnd, video::EDT_DIRECT3D9);
-	if (device == NULL) {
-		device = create(hwnd, video::EDT_DIRECT3D8);
-		if (device == NULL) {
-			device = create(hwnd, video::EDT_OPENGL);
-		}
-	}
-	if (device == NULL) {
-		TVPThrowExceptionMessage(L"Irrlicht デバイスの初期化に失敗しました");
-	}
-
-	//device->setWindowCaption(title.c_str());
-	driver  = device->getVideoDriver();
-	smgr    = device->getSceneManager();
-	texture = driver->addTexture(core::dimension2d<s32>(1024, 1024), "layer", ECF_A8R8G8B8);
-
-	// XXX テスト用
-	init();
-	
-	// 駆動開始
-	start();
-}
-
-/**
- * ウインドウの解除
- */
-void
-tTVPIrrlichtDrawDevice::detach()
-{
-	stop();
-	if (device) {
-		device->drop();
-		device = NULL;
-		driver = NULL;
-		smgr   = NULL;
-		texture = NULL;
-		destBuffer = NULL;
-	}
-}
-
-/***
- * ウインドウの指定
- * @param wnd ウインドウハンドラ
- */
-void TJS_INTF_METHOD
-tTVPIrrlichtDrawDevice::SetTargetWindow(HWND wnd)
-{
-	detach();
-	if (wnd != NULL) {
-		attach(wnd);
-	}
-}
-
-/**
- * ビットマップコピー処理開始
- */
-void TJS_INTF_METHOD
-tTVPIrrlichtDrawDevice::StartBitmapCompletion(iTVPLayerManager * manager)
-{
-	// bitmap処理開始
-	if (device && texture) {
+	if (texture) {
 		destBuffer = (unsigned char *)texture->lock();
 		core::dimension2d<s32> size = texture->getSize();
 		destWidth  = size.Width;
 		destHeight = size.Height;
 		destPitch  = texture->getPitch();
+	} else {
+		destBuffer = NULL;
 	}
 }
 
 /**
- * ビットマップコピー処理
+ * ロックされたテクスチャにビットマップ描画を行う
  */
-void TJS_INTF_METHOD
-tTVPIrrlichtDrawDevice::NotifyBitmapCompleted(iTVPLayerManager * manager,
-	tjs_int x, tjs_int y, const void * bits, const BITMAPINFO * bitmapinfo,
-	const tTVPRect &cliprect, tTVPLayerType type, tjs_int opacity)
+void
+LayerManagerInfo::copy(tjs_int x, tjs_int y, const void * bits, const BITMAPINFO * bitmapinfo,
+					   const tTVPRect &cliprect, tTVPLayerType type, tjs_int opacity)
 {
 	// bits, bitmapinfo で表されるビットマップの cliprect の領域を、x, y に描画する。
-	if (device && texture && destBuffer) {
+
+	if (destBuffer) {
 		int srcPitch = -bitmapinfo->bmiHeader.biWidth * 4; // XXX きめうち
 		unsigned char *srcBuffer = (unsigned char *)bits - srcPitch * (bitmapinfo->bmiHeader.biHeight - 1);
 		int srcx   = cliprect.left;
@@ -166,15 +79,216 @@ tTVPIrrlichtDrawDevice::NotifyBitmapCompleted(iTVPLayerManager * manager,
 }
 
 /**
+ * テクスチャのロックの解除
+ */
+void
+LayerManagerInfo::unlock()
+{
+	if (texture) {
+		texture->unlock();
+		destBuffer = NULL;
+	}
+}
+
+/**
+ * コンストラクタ
+ */
+tTVPIrrlichtDrawDevice::tTVPIrrlichtDrawDevice()
+{
+	device = NULL;
+}
+
+/**
+ * デストラクタ
+ */
+tTVPIrrlichtDrawDevice::~tTVPIrrlichtDrawDevice()
+{
+	detach();
+}
+
+/**
+ * テクスチャの割り当て
+ * @param manager レイヤマネージャ
+ */
+void 
+tTVPIrrlichtDrawDevice::allocInfo(iTVPLayerManager * manager)
+{
+	if (device) {
+		video::IVideoDriver* driver = device->getVideoDriver();
+
+		// テクスチャ割り当てXXX サイズ判定が必要…
+		ITexture *texture = driver->addTexture(core::dimension2d<s32>(1024, 1024), "layer", ECF_A8R8G8B8);
+		if (texture == NULL) {
+			TVPThrowExceptionMessage(L"テクスチャの割り当てに失敗しました");
+		}
+
+		manager->SetDrawDeviceData((void*)new LayerManagerInfo(texture));
+	}
+}
+
+/**
+ *  テクスチャの解放
+ * @param manager レイヤマネージャ
+ */
+void
+tTVPIrrlichtDrawDevice::freeInfo(iTVPLayerManager * manager)
+{
+	if (device) {
+		LayerManagerInfo *info = (LayerManagerInfo*)manager->GetDrawDeviceData();
+		if (info != NULL) {
+			video::IVideoDriver* driver = device->getVideoDriver();
+			driver->removeTexture(info->texture);
+			manager->SetDrawDeviceData(NULL);
+			delete info;
+		}
+	}
+}
+
+/**
+ * デバイスの生成
+ * @param hwnd ハンドル
+ * @param ドライバの種類
+ */
+IrrlichtDevice *
+tTVPIrrlichtDrawDevice::create(HWND hwnd, video::E_DRIVER_TYPE type)
+{
+	SIrrlichtCreationParameters params;
+	params.WindowId     = reinterpret_cast<s32>(hwnd);
+	params.DriverType    = type;
+//	params.WindowSize    = core::dimension2d<s32>(width, height);
+	params.Bits          = 32;
+	params.Stencilbuffer = true;
+	params.Vsync = true;
+	params.EventReceiver = this;
+	return createDeviceEx(params);
+}
+
+/**
+ * ウインドウの再設定
+ * @param hwnd ハンドル
+ */
+void
+tTVPIrrlichtDrawDevice::attach(HWND hwnd)
+{
+	// ウインドウサイズの取得 XXX
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+	width  = rect.right - rect.left;
+	height = rect.bottom - rect.top;
+
+	// デバイス生成
+	device = create(hwnd, video::EDT_DIRECT3D9);
+	if (device == NULL) {
+		device = create(hwnd, video::EDT_DIRECT3D8);
+		if (device == NULL) {
+			device = create(hwnd, video::EDT_OPENGL);
+		}
+	}
+	if (device == NULL) {
+		TVPThrowExceptionMessage(L"Irrlicht デバイスの初期化に失敗しました");
+	}
+
+	//device->setWindowCaption(title.c_str());
+	
+	// マネージャに対するテクスチャの割り当て
+	for (std::vector<iTVPLayerManager *>::iterator i = Managers.begin(); i != Managers.end(); i++) {
+		allocInfo(*i);
+	}
+
+	// XXX テスト用
+	init();
+	
+	// 駆動開始
+	start();
+}
+
+/**
+ * ウインドウの解除
+ */
+void
+tTVPIrrlichtDrawDevice::detach()
+{
+	if (device) {
+		stop();
+		for (std::vector<iTVPLayerManager *>::iterator i = Managers.begin(); i != Managers.end(); i++) {
+			freeInfo(*i);
+		}
+		device->drop();
+		device = NULL;
+	}
+}
+
+/**
+ * レイヤマネージャの登録
+ * @param manager レイヤマネージャ
+ */
+void TJS_INTF_METHOD
+tTVPIrrlichtDrawDevice::AddLayerManager(iTVPLayerManager * manager)
+{
+	allocInfo(manager);
+	tTVPDrawDevice::AddLayerManager(manager);
+}
+
+/**
+ * レイヤマネージャの削除
+ * @param manager レイヤマネージャ
+ */
+void TJS_INTF_METHOD
+tTVPIrrlichtDrawDevice::RemoveLayerManager(iTVPLayerManager * manager)
+{
+	freeInfo(manager);
+	tTVPDrawDevice::RemoveLayerManager(manager);
+}
+
+
+/***
+ * ウインドウの指定
+ * @param wnd ウインドウハンドラ
+ */
+void TJS_INTF_METHOD
+tTVPIrrlichtDrawDevice::SetTargetWindow(HWND wnd)
+{
+	detach();
+	if (wnd != NULL) {
+		attach(wnd);
+	}
+}
+
+/**
+ * ビットマップコピー処理開始
+ */
+void TJS_INTF_METHOD
+tTVPIrrlichtDrawDevice::StartBitmapCompletion(iTVPLayerManager * manager)
+{
+	LayerManagerInfo *info = (LayerManagerInfo*)manager->GetDrawDeviceData();
+	if (info) {
+		info->lock();
+	}
+}
+
+/**
+ * ビットマップコピー処理
+ */
+void TJS_INTF_METHOD
+tTVPIrrlichtDrawDevice::NotifyBitmapCompleted(iTVPLayerManager * manager,
+	tjs_int x, tjs_int y, const void * bits, const BITMAPINFO * bitmapinfo,
+	const tTVPRect &cliprect, tTVPLayerType type, tjs_int opacity)
+{
+	LayerManagerInfo *info = (LayerManagerInfo*)manager->GetDrawDeviceData();
+	if (info) {
+		info->copy(x, y, bits, bitmapinfo, cliprect, type, opacity);
+	}
+}
+
+/**
  * ビットマップコピー処理終了
  */
 void TJS_INTF_METHOD
 tTVPIrrlichtDrawDevice::EndBitmapCompletion(iTVPLayerManager * manager)
 {
-	// bitmap 処理終了
-	if (device && texture) {
-		texture->unlock();
-		destBuffer = NULL;
+	LayerManagerInfo *info = (LayerManagerInfo*)manager->GetDrawDeviceData();
+	if (info) {
+		info->unlock();
 	}
 }
 
@@ -186,7 +300,10 @@ tTVPIrrlichtDrawDevice::EndBitmapCompletion(iTVPLayerManager * manager)
 void
 tTVPIrrlichtDrawDevice::init()
 {
-#if 0
+	/// シーンマネージャ
+	ISceneManager* smgr = device->getSceneManager();
+
+#if 1
 	// 以下サンプルデータのロード処理
 	ttstr dataPath = "../../../media/sydney.md2";
 	dataPath = TVPNormalizeStorageName(dataPath);
@@ -219,8 +336,8 @@ tTVPIrrlichtDrawDevice::init()
 	if (node)
 		node->setPosition(core::vector3df(-1300,-144,-1249));
 	
-	smgr->addCameraSceneNodeFPS();
-	device->getCursorControl()->setVisible(false);
+//	smgr->addCameraSceneNodeFPS();
+//	device->getCursorControl()->setVisible(false);
 #endif
 }
 
@@ -254,14 +371,54 @@ tTVPIrrlichtDrawDevice::OnContinuousCallback(tjs_uint64 tick)
 {
 	if (device) {
 		device->getTimer()->tick();
-		driver->beginScene(true, true, video::SColor(255,100,101,140));
+
+		/// ドライバ
+		video::IVideoDriver* driver = device->getVideoDriver();
+
+		// 描画開始
+		driver->beginScene(true, true, video::SColor(255,0,0,0));
+
+		/// シーンマネージャ
+		ISceneManager* smgr = device->getSceneManager();
 		smgr->drawAll();
-		if (texture) {
-			driver->draw2DImage(texture, core::position2d<s32>(0,0),
-								core::rect<s32>(0,0,width,height), 0, 
-								video::SColor(255,255,255,255), true);
+
+		// 個別レイヤマネージャの描画
+		for (std::vector<iTVPLayerManager *>::iterator i = Managers.begin(); i != Managers.end(); i++) {
+			LayerManagerInfo *info = (LayerManagerInfo*)(*i)->GetDrawDeviceData();
+			if (info && info->texture) {
+				driver->draw2DImage(info->texture, core::position2d<s32>(0,0),
+									core::rect<s32>(0,0,width,height), 0, 
+									video::SColor(255,255,255,255), true);
+			}
 		}
+		// 描画完了
 		driver->endScene();
 	}
 };
 
+/**
+ * イベント受理
+ * @param event イベント情報
+ * @return 処理したら true
+ */
+bool
+tTVPIrrlichtDrawDevice::OnEvent(SEvent event)
+{
+	switch (event.EventType) {
+	case EET_KEY_INPUT_EVENT:
+		int shift = 0;
+		if (event.KeyInput.Shift) {
+			shift |= 0x01;
+		}
+		if (event.KeyInput.Control) {
+			shift |= 0x04;
+		}
+		if (event.KeyInput.PressedDown){
+			OnKeyDown(event.KeyInput.Key, shift); 
+	    } else {
+			OnKeyUp(event.KeyInput.Key, shift);
+        }
+        return true;
+	}
+	return false;
+}
