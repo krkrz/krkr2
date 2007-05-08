518bdbe9bbe695332f1f62ec946e43eed9f53d02
wtnbgo <go@dsl.gr.jp>
2007-05-08 03:45:00 +0000

    レイヤムービー拡張プラグイン
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src@2634 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ plugins/win32/layerExMovie/layerExMovie.hpp
@@ -0,0 +1,55 @@
+#ifndef _layExMovie_hpp_
+#define _layExMovie_hpp_
+
+#include <windows.h>
+#include <dshow.h> 
+#include <mmstream.h> 
+#include <amstream.h> 
+#include <ddstream.h> 
+
+#include "layerExBase.hpp"
+
+/*
+ * Movie 描画用レイヤ
+ */
+struct layerExMovie : public layerExBase, public tTVPContinuousEventCallbackIntf
+{
+protected:
+	IAMMultiMediaStream*     pAMStream; 
+	IMediaStream*            pPrimaryVidStream; 
+	IDirectDrawMediaStream*  pDDStream; 
+	IDirectDrawStreamSample* pSample; 
+	IDirectDrawSurface*      pSurface; 
+
+	ObjectT _pType;
+
+	int movieWidth;
+	int movieHeight;
+	
+	bool loop;
+	bool alpha;
+	
+public:
+	layerExMovie(DispatchT obj);
+	~layerExMovie();
+	
+public:
+	
+	// ムービーのロード
+	void openMovie(const tjs_char* filename, bool alpha);
+
+	void startMovie(bool loop);
+	void stopMovie();
+	
+	void start();
+	void stop();
+	
+	/**
+	 * Continuous コールバック
+	 * 吉里吉里が暇なときに常に呼ばれる
+	 * 塗り直し処理
+	 */
+	virtual void TJS_INTF_METHOD OnContinuousCallback(tjs_uint64 tick);
+};
+
+#endif
