0bf91e8150aa1ba4a7a30e5732131d1e46cd1b62
wtnbgo <go@dsl.gr.jp>
2007-06-19 19:40:49 +0000

    吉里吉里側にオブジェクトの squirrel 形式での保存機能を追加
    git-svn-id: svn://localhost@2883 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/plugins/win32/squirrel/Main.cpp
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/squirrel/Main.cpp
@@ -36,6 +36,151 @@ extern void unregistglobal(HSQUIRRELVM v, const SQChar *name);
 
 //---------------------------------------------------------------------------
 
+#include "../json/Writer.hpp"
+
+static void
+quoteString(const tjs_char *str, IWriter *writer)
+{
+	if (str) {
+		writer->write((tjs_char)'"');
+		const tjs_char *p = str;
+		int ch;
+		while ((ch = *p++)) {
+			if (ch == '"') {
+				writer->write(L"\\\"");
+			} else {
+				writer->write((tjs_char)ch);
+			}
+		}
+		writer->write((tjs_char)'"');
+	} else {
+		writer->write(L"null");
+	}
+}
+
+static void getVariantString(tTJSVariant &var, IWriter *writer);
+
+/**
+ * 辞書の内容表示用の呼び出しロジック
+ */
+class DictMemberDispCaller : public tTJSDispatch /** EnumMembers 用 */
+{
+protected:
+	IWriter *writer;
+	bool first;
+public:
+	DictMemberDispCaller(IWriter *writer) : writer(writer) { first = true; };
+	virtual tjs_error TJS_INTF_METHOD FuncCall( // function invocation
+												tjs_uint32 flag,			// calling flag
+												const tjs_char * membername,// member name ( NULL for a default member )
+												tjs_uint32 *hint,			// hint for the member name (in/out)
+												tTJSVariant *result,		// result
+												tjs_int numparams,			// number of parameters
+												tTJSVariant **param,		// parameters
+												iTJSDispatch2 *objthis		// object as "this"
+												) {
+		if (numparams > 1) {
+			if ((int)param[1] != TJS_HIDDENMEMBER) {
+				if (first) {
+					first = false;
+				} else {
+					writer->write((tjs_char)',');
+					writer->newline();
+				}
+				writer->write((tjs_char)'[');
+				quoteString(param[0]->GetString(), writer);
+				writer->write(L"]=");
+				getVariantString(*param[2], writer);
+			}
+		}
+		if (result) {
+			*result = true;
+		}
+		return TJS_S_OK;
+	}
+};
+
+static void getDictString(iTJSDispatch2 *dict, IWriter *writer)
+{
+	writer->write((tjs_char)'{');
+	writer->addIndent();
+	DictMemberDispCaller caller(writer);
+	tTJSVariantClosure closure(&caller);
+	dict->EnumMembers(TJS_IGNOREPROP, &closure, dict);
+	writer->delIndent();
+	writer->write((tjs_char)'}');
+}
+
+// Array クラスメンバ
+static iTJSDispatch2 *ArrayCountProp   = NULL;   // Array.count
+
+static void getArrayString(iTJSDispatch2 *array, IWriter *writer)
+{
+	writer->write((tjs_char)'[');
+	writer->addIndent();
+	tjs_int64 count = 0;
+	{
+		tTJSVariant result;
+		if (TJS_SUCCEEDED(ArrayCountProp->PropGet(0, NULL, NULL, &result, array))) {
+			count = result.AsInteger();
+		}
+	}
+	for (int i=0; i<count; i++) {
+		if (i != 0) {
+			writer->write((tjs_char)',');
+			writer->newline();
+		}
+		tTJSVariant result;
+		if (array->PropGetByNum(TJS_IGNOREPROP, i, &result, array) == TJS_S_OK) {
+			getVariantString(result, writer);
+		}
+	}
+	writer->delIndent();
+	writer->write((tjs_char)']');
+}
+
+static void
+getVariantString(tTJSVariant &var, IWriter *writer)
+{
+	switch(var.Type()) {
+
+	case tvtVoid:
+		writer->write(L"null");
+		break;
+		
+	case tvtObject:
+		{
+			iTJSDispatch2 *obj = var.AsObjectNoAddRef();
+			if (obj == NULL) {
+				writer->write(L"null");
+			} else if (obj->IsInstanceOf(TJS_IGNOREPROP,NULL,NULL,L"Array",obj) == TJS_S_TRUE) {
+				getArrayString(obj, writer);
+			} else {
+				getDictString(obj, writer);
+			}
+		}
+		break;
+		
+	case tvtString:
+		quoteString(var.GetString(), writer);
+		break;
+
+	case tvtInteger:
+		writer->write((tTVInteger)var);
+		break;
+
+	case tvtReal:
+		writer->write((tTVReal)var);
+		break;
+
+	default:
+		writer->write(L"null");
+		break;
+	};
+}
+
+//---------------------------------------------------------------------------
+
 /**
  * Scripts クラスへの Squirrel 実行メソッドの追加
  */
@@ -96,11 +241,42 @@ public:
 		stream->Destruct();
 		return S_OK;
 	}
+
+	/**
+	 * squirrel 形式でのオブジェクトの保存
+	 * @param filename ファイル名
+	 * @return 実行結果
+	 */
+	static tjs_error TJS_INTF_METHOD save(tTJSVariant *result,
+										  tjs_int numparams,
+										  tTJSVariant **param,
+										  iTJSDispatch2 *objthis) {
+		if (numparams < 2) return TJS_E_BADPARAMCOUNT;
+		IFileWriter writer(param[0]->GetString());
+		writer.write(L"return ");
+		getVariantString(*param[1], &writer);
+		return TJS_S_OK;
+	}
+
+	static tjs_error TJS_INTF_METHOD toString(tTJSVariant *result,
+											  tjs_int numparams,
+											  tTJSVariant **param,
+											  iTJSDispatch2 *objthis) {
+		if (numparams < 1) return TJS_E_BADPARAMCOUNT;
+		if (result) {
+			IStringWriter writer;
+			getVariantString(*param[0], &writer);
+			*result = writer.buf;
+		}
+		return TJS_S_OK;
+	}
 };
 
 NCB_ATTACH_CLASS(ScriptsSquirrel, Scripts) {
 	RawCallback("execSQ",        &ScriptsSquirrel::exec,        TJS_STATICMEMBER);
 	RawCallback("execStorageSQ", &ScriptsSquirrel::execStorage, TJS_STATICMEMBER);
+	RawCallback("saveSQ",        &ScriptsSquirrel::save,        TJS_STATICMEMBER);
+	RawCallback("toSQString",    &ScriptsSquirrel::toString,    TJS_STATICMEMBER);
 };
 
 /**
@@ -212,6 +388,22 @@ static void PostRegistCallback()
 		registglobal(SquirrelVM::GetVMPtr(), KIRIKIRI_GLOBAL, global);
 		global->Release();
 	}
+
+	// Array.count を取得
+	{
+		tTJSVariant varScripts;
+		TVPExecuteExpression(TJS_W("Array"), &varScripts);
+		iTJSDispatch2 *dispatch = varScripts.AsObjectNoAddRef();
+		tTJSVariant val;
+		if (TJS_FAILED(dispatch->PropGet(TJS_IGNOREPROP,
+										 TJS_W("count"),
+										 NULL,
+										 &val,
+										 dispatch))) {
+			TVPThrowExceptionMessage(L"can't get Array.count");
+		}
+		ArrayCountProp = val.AsObject();
+	}
 }
 
 /**
@@ -219,6 +411,11 @@ static void PostRegistCallback()
  */
 static void PreUnregistCallback()
 {
+	if (ArrayCountProp) {
+		ArrayCountProp->Release();
+		ArrayCountProp = NULL;
+	}
+	
 	// TJS のグローバルオブジェクトを取得する
 	iTJSDispatch2 * global = TVPGetScriptDispatch();
 	if (global)	{
