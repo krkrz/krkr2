5ce40e81d694253f669e858f4917836782ad3b58
wtnbgo <go@dsl.gr.jp>
2007-10-05 04:56:50 +0000

    libpsd 動作状態
    git-svn-id: svn://localhost@3319 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/libpsd/libpsd/src/psd_system.cpp
@@ -0,0 +1,81 @@
+// created: 2007-01-27
+#include <stdio.h>
+#include <stdlib.h>
+#include "libpsd.h"
+#include "psd_system.h"
+
+// XXX support for kirikiri
+#include <windows.h>
+#include "../../../tp_stub.h"
+
+void * psd_malloc(psd_int size)
+{
+	return TVP_malloc(size);
+}
+
+void * psd_realloc(void * block, psd_int size)
+{
+	return TVP_realloc(block, size);
+}
+
+void psd_free(void * block)
+{
+	TVP_free(block);
+}
+
+void psd_freeif(void * block)
+{
+	if (block != NULL)
+		psd_free(block);
+}
+
+void * psd_fopen(psd_char * file_name)
+{
+	return (void *)TVPCreateIStream(file_name, TJS_BS_READ);
+}
+
+psd_int psd_fsize(void * file)
+{
+	IStream *is = (IStream *)file;
+	if (is) {
+		STATSTG stat;
+		is->Stat(&stat, STATFLAG_NONAME);
+		return (psd_int)stat.cbSize.QuadPart;
+	}
+	return 0;
+}
+
+psd_int psd_fread(psd_uchar * buffer, psd_int count, void * file)
+{
+	IStream *is = (IStream *)file;
+	if (is) {
+		ULONG len;
+		if (is->Read(buffer,count,&len) == S_OK) {
+			return len;
+		} 
+	}
+	return 0;
+}
+
+psd_int psd_fseek(void * file, psd_int length)
+{
+	IStream *is = (IStream *)file;
+	if (is) {
+		LARGE_INTEGER move;
+		move.QuadPart = length;
+		ULARGE_INTEGER newposition;
+		if (is->Seek(move, STREAM_SEEK_CUR, &newposition) == S_OK) {
+			return (psd_int)newposition.QuadPart;
+		}
+	}
+	return -1;
+}
+
+void psd_fclose(void * file)
+{
+	IStream *is = (IStream *)file;
+	if (is) {
+		is->Release();
+	}
+}
+
