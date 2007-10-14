0fd485b043c3c10cd997e3209e7941a6678e38f0
w-dee <w-dee@users.noreply.github.com>
2007-10-14 03:20:24 +0000

    DirectDraw のドライバの情報を、DirectDraw を読み込んだだけでは取得しないようにした (アホWindowsがcrl.microsoft.comに問い合わせに行ってしまうため)
    git-svn-id: svn://localhost/kirikiri2/trunk@3350 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/src/core/environ/win32/VersionFormUnit.cpp
+++ kirikiri2/src/core/environ/win32/VersionFormUnit.cpp
@@ -14,6 +14,7 @@
 #include "VersionFormUnit.h"
 #include "ConsoleFormUnit.h"
 #include "MainFormUnit.h"
+#include "WindowImpl.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -26,12 +27,12 @@ __fastcall TTVPVersionForm::TTVPVersionForm(TComponent* Owner)
 //---------------------------------------------------------------------------
 void __fastcall TTVPVersionForm::PopupMenuPopup(TObject *Sender)
 {
-    CopyMenuItem->Enabled = Memo->SelLength != 0;
+	CopyMenuItem->Enabled = Memo->SelLength != 0;
 }
 //---------------------------------------------------------------------------
 void __fastcall TTVPVersionForm::CopyMenuItemClick(TObject *Sender)
 {
-    Memo->CopyToClipboard();
+	Memo->CopyToClipboard();
 }
 //---------------------------------------------------------------------------
 void __fastcall TTVPVersionForm::CopyEnvInfoButtonClick(TObject *Sender)
@@ -43,15 +44,19 @@ void __fastcall TTVPVersionForm::CopyEnvInfoButtonClick(TObject *Sender)
 //---------------------------------------------------------------------------
 void TVPShowVersionForm()
 {
-    // show version information
-    TTVPVersionForm *form = new TTVPVersionForm(Application);
-    TStringList *list = new TStringList();
-    list->Text = TVPGetAboutString().AsAnsiString();
-    form->Memo->Lines->Assign(list);
-    form->Memo->SelStart = 0;
-    delete list;
-    form->ShowModal();
-    delete form;
+	// get DirectDraw driver information
+	TVPEnsureDirectDrawObject();
+	TVPDumpDirectDrawDriverInformation();
+
+	// show version information
+	TTVPVersionForm *form = new TTVPVersionForm(Application);
+	TStringList *list = new TStringList();
+	list->Text = TVPGetAboutString().AsAnsiString();
+	form->Memo->Lines->Assign(list);
+	form->Memo->SelStart = 0;
+	delete list;
+	form->ShowModal();
+	delete form;
 }
 //---------------------------------------------------------------------------
 
