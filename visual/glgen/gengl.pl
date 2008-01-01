5432231c93fd26ef996594ed62defd903a51d806
w-dee <w-dee@users.noreply.github.com>
2008-01-01 13:55:59 +0000

    Copyright付け替え祭り '08
    あけましておめでとうございます。今年もよろしくお願い致します。
    
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src/core@3500 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- visual/glgen/gengl.pl
+++ visual/glgen/gengl.pl
@@ -6,7 +6,7 @@
 $gpl = <<EOF;
 
 	TVP2 ( T Visual Presenter 2 )  A script authoring tool
-	Copyright (C) 2000-2007 W.Dee <dee\@kikyou.info> and contributors
+	Copyright (C) 2000-2008 W.Dee <dee\@kikyou.info> and contributors
 
 	See details of license at "license.txt"
 
