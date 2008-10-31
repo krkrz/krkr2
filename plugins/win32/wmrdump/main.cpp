65b885890ed60d501cb576bc82a4ece87d7f0ece
miahmie <miahmie@users.noreply.github.com>
2008-10-31 14:11:21 +0000

    wmrdumpを間違ってコミットしてしまったので戻しますorz
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src@4254 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- plugins/win32/wmrdump/main.cpp
+++ plugins/win32/wmrdump/main.cpp
@@ -163,7 +163,7 @@ int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved
 }
 //---------------------------------------------------------------------------
 static tjs_int GlobalRefCountAtInit = 0;
-extern "C" __declspec(dllexport) HRESULT __stdcall V2Link(iTVPFunctionExporter *exporter)
+extern "C" HRESULT _stdcall _export V2Link(iTVPFunctionExporter *exporter)
 {
 	// スタブの初期化(必ず記述する)
 	TVPInitImportStub(exporter);
@@ -236,7 +236,7 @@ extern "C" __declspec(dllexport) HRESULT __stdcall V2Link(iTVPFunctionExporter *
 	return S_OK;
 }
 //---------------------------------------------------------------------------
-extern "C" __declspec(dllexport) HRESULT __stdcall V2Unlink()
+extern "C" HRESULT _stdcall _export V2Unlink()
 {
 	// 吉里吉里側から、プラグインを解放しようとするときに呼ばれる関数。
 
