ae05c3776262124b7864fe4b7d93e6187540c4ef
YunYunDetective <yun@nekome.net>
2009-01-19 13:45:08 +0000

    LayerExAVIに openCompressedAVI()関数を追加。 圧縮指定を行った上でAVIキャプチャが行えるようにした。
    git-svn-id: svn://localhost/kirikiri2/trunk/kirikiri2/src@4375 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- plugins/win32/layerExAVI/Main.cpp
+++ plugins/win32/layerExAVI/Main.cpp
@@ -14,6 +14,10 @@ protected:
 	// 動画記録用の情報
 	PAVIFILE      pavi; //< AVIファイル
 	PAVISTREAM    pstm; //< AVIストリーム
+        PAVISTREAM    ptmp; //< 圧縮ストリーム
+        COMPVARS cv;
+        bool hasCv;
+        AVICOMPRESSOPTIONS opt;
 	int aviWidth;  //< 開始時横幅
 	int aviHeight; //< 開始時縦幅
 	int lastFrame; //< 最終録画フレーム
@@ -61,7 +65,7 @@ public:
 	/**
 	 * コンストラクタ
 	 */
-	LayerExAVI(DispatchT obj) : layerExBase(obj), pavi(NULL), pstm(NULL), hwi(NULL), wvout(NULL) {
+	LayerExAVI(DispatchT obj) : layerExBase(obj), pavi(NULL), pstm(NULL), ptmp(NULL), hwi(NULL), wvout(NULL) {
 		ZeroMemory(&wvhdr, sizeof wvhdr);
 	}
 
@@ -119,7 +123,8 @@ public:
 		if (AVIFileCreateStream(pavi, &pstm, &si) != 0) {
 			closeAVI();
 			TVPThrowExceptionMessage(L"AVIFileCreateStream");
-		}
+                }
+
 
 		// ストリームに投げ込むデータフォーマットを指定
 
@@ -142,7 +147,118 @@ public:
 		}
 
 		// 先頭フレーム
+                lastFrame = -1;
+
+                hasCv = false;
+        }
+
+	/**
+	 * AVIファイルを圧縮フォーマットを指定して開く
+	 * @param filename 保存ファイル名
+	 * @param fps 秒間フレーム数
+         * @param return 圧縮ダイアログでキャンセルを押した場合false。
+	 */
+	bool openCompressedAVI(const tjs_char *filename, int fps) {
+
+		closeAVI();
+
+		// 録画開始時のサイズを記録しておく
+		aviWidth  = _width;
+		aviHeight = _height;
+
+		// ストリームに投げ込むデータフォーマットを指定
+
+		BITMAPINFOHEADER bih;
+		bih.biSize = sizeof(bih);
+		bih.biWidth  = aviWidth;
+		bih.biHeight = aviHeight;
+		bih.biPlanes = 1;
+		bih.biBitCount = 32;
+		bih.biCompression = BI_RGB;
+		bih.biSizeImage = 0;
+		bih.biXPelsPerMeter = 0;
+		bih.biYPelsPerMeter = 0;
+		bih.biClrUsed = 0;
+		bih.biClrImportant = 0;
+
+                // 圧縮オプションを取得
+                memset(&cv,0,sizeof(COMPVARS));
+                cv.cbSize=sizeof(COMPVARS);
+                cv.dwFlags=ICMF_COMPVARS_VALID;
+                cv.fccHandler=comptypeDIB;
+                cv.lQ=ICQUALITY_DEFAULT;
+                if (!ICCompressorChoose(NULL,ICMF_CHOOSE_DATARATE | ICMF_CHOOSE_KEYFRAME,
+                                        &bih,NULL,&cv,NULL)) {
+                  return false;
+                }
+
+                // オプションを指定
+                opt.fccType=streamtypeVIDEO;
+                opt.fccHandler=cv.fccHandler;
+                opt.dwKeyFrameEvery=cv.lKey;
+                opt.dwQuality=cv.lQ;
+                opt.dwBytesPerSecond=cv.lDataRate;
+                opt.dwFlags=(cv.lDataRate>0?AVICOMPRESSF_DATARATE:0)
+                  |(cv.lKey>0?AVICOMPRESSF_KEYFRAMES:0);
+                opt.lpFormat=NULL;
+                opt.cbFormat=0;
+                opt.lpParms=cv.lpState;
+                opt.cbParms=cv.cbState;
+                opt.dwInterleaveEvery=0;
+                
+
+		// AVIファイルを開く
+		ttstr path = TVPNormalizeStorageName(ttstr(filename));
+		TVPGetLocalName(path);
+		if (AVIFileOpen(&pavi, path.c_str(), OF_CREATE | OF_WRITE | OF_SHARE_DENY_NONE,NULL) != 0)	{
+			ttstr msg = filename;
+			msg += ":can't open";
+			TVPThrowExceptionMessage(msg.c_str());
+		}
+
+		// AVIストリームの生成
+		AVISTREAMINFO si = {	
+			streamtypeVIDEO, // Video Stream
+			comptypeDIB,
+			0,               // Stream Flag
+			0,
+			0,
+			0,
+			1,               // 時間単位 dwScale
+			fps,             // フレーム dwRate
+			0,
+			10,         // ストリームの長さ XXX
+			0,
+			0,
+			(DWORD)-1,       // -1: Default品質 [0-10000]
+			0,
+			// 表示する矩形サイズ
+			{ 0, 0, aviWidth, aviHeight },
+			0,
+			0,
+			L"KIRIKIRI" };
+                si.fccHandler=cv.fccHandler;
+
+		if (AVIFileCreateStream(pavi, &pstm, &si) != 0) {
+			closeAVI();
+			TVPThrowExceptionMessage(L"AVIFileCreateStream");
+		}
+                if (AVIMakeCompressedStream(&ptmp,pstm,&opt,NULL)!=AVIERR_OK) {
+                  closeAVI();
+                  TVPThrowExceptionMessage(L"AVIMakeCompressedStream");
+                }
+
+		if (AVIStreamSetFormat(ptmp, 0, &bih, sizeof(bih)) != 0 ) {
+			closeAVI();
+			TVPThrowExceptionMessage(L"AVIFileCreateStream");
+		}
+
+		// 先頭フレーム
 		lastFrame = -1;
+
+                hasCv = true;
+
+                return true;
 	}
 
 	/**
@@ -159,7 +275,7 @@ public:
 				// 吉里吉里のバッファは DIB と同じ構造なのでこの処理で通る
 				int size = _height * -_pitch;
 				const unsigned char *buffer = _buffer + (_height-1) * _pitch;
-				if (AVIStreamWrite(pstm, frame, 1, (void*)buffer, size, AVIIF_KEYFRAME, NULL, NULL ) != 0) {
+				if (AVIStreamWrite(hasCv ? ptmp : pstm, frame, 1, (void*)buffer, size, AVIIF_KEYFRAME, NULL, NULL ) != 0) {
 					TVPThrowExceptionMessage(L"AVIStreamWrite");
 				}
 				lastFrame = frame;
@@ -173,6 +289,10 @@ public:
 	 * AVIファイルを閉じる
 	 */
 	void closeAVI() {
+                if (ptmp) {
+                   AVIStreamRelease(ptmp);
+                   ptmp = NULL;
+                }
 		if (pstm) {
 			AVIStreamRelease(pstm);
 			pstm = NULL;
@@ -181,6 +301,9 @@ public:
 			AVIFileRelease(pavi);
 			pavi = NULL;
 		}
+                if (hasCv) {
+                  ICCompressorFree(&cv);
+                }
 	}
 
 	/**
@@ -290,6 +413,7 @@ NCB_GET_INSTANCE_HOOK(LayerExAVI)
 
 NCB_ATTACH_CLASS_WITH_HOOK(LayerExAVI, Layer) {
 	NCB_METHOD(openAVI);
+	NCB_METHOD(openCompressedAVI);
 	NCB_METHOD(closeAVI);
 	NCB_METHOD(recordAVI);
 	NCB_METHOD(openWAV);
