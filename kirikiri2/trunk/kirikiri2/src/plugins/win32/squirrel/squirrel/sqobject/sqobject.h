fd359d8a250e721033a189ff7d0caa4d0f52a05b
wtnbgo <go@dsl.gr.jp>
2009-05-02 17:53:16 +0000

    最新追従
    git-svn-id: svn://localhost@4444 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/plugins/win32/squirrel/squirrel/sqobject/sqobject.h
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/squirrel/squirrel/sqobject/sqobject.h
@@ -57,6 +57,14 @@ public:
 	 */
 	virtual ~Object();
 
+	/**
+	 * 自己参照初期化用
+	 * デフォルトコンストラクタで処理した場合は必ずこの処理をよぶこと
+	 * @param v SQUIRREL vm
+	 * @param idx 自分のオブジェクトがあるインデックス
+	 */
+	void initSelf(HSQUIRRELVM v, int idx=1);
+
 public:
 	
 	// ------------------------------------------------------------------
@@ -107,6 +115,67 @@ public:
 	 * squirrel クラス登録
 	 */
 	static void registerClass();
+
+
+protected:
+
+	/**
+	 * 自己オブジェクトイベント呼び出し（引数無し)
+	 * @param eventName イベント名
+	 */
+	SQRESULT callEvent(const SQChar *eventName) {
+		return self.callMethod(eventName);
+	}
+
+	/**
+	 * 自己オブジェクトイベント呼び出し（引数1つ)
+	 * @param eventName イベント名
+	 * @param p1 引数
+	 */
+	template<typename T1> SQRESULT callEvent(const SQChar *eventName, T1 p1) {
+		return self.callMethod(eventName, p1);
+	}
+	
+	/**
+	 * 自己オブジェクトイベント呼び出し（引数2つ)
+	 * @param eventName イベント名
+	 * @param p1 引数
+	 * @param p2 引数2
+	 */
+	template<typename T1, typename T2> SQRESULT callEvent(const SQChar *eventName, T1 p1, T2 p2) {
+		return self.callMethod(eventName, p1, p2);
+	}
+	
+	/**
+	 * 返値有り自己オブジェクトイベント呼び出し（引数無し)
+	 * @param r 帰り値ポインタ
+	 * @param eventName イベント名
+	 */
+	template<typename R> SQRESULT callEventResult(R* r, const SQChar *eventName) {
+		return self.callMethodResult(r, eventName);
+	}
+
+	/**
+	 * 返値あり自己オブジェクトイベント呼び出し（引数1つ)
+	 * @param r 帰り値ポインタ
+	 * @param eventName イベント名
+	 * @param p1 引数
+	 */
+	template<typename R, typename T1> SQRESULT callEventResult(R* r, const SQChar *eventName, T1 p1) {
+		return self.callMethodResult(r, eventName, p1);
+	}
+	
+	/**
+	 * 返値有り自己オブジェクトイベント呼び出し（引数2つ)
+	 * @param r 帰り値ポインタ
+	 * @param eventName イベント名
+	 * @param p1 引数
+	 * @param p2 引数2
+	 */
+	template<typename R, typename T1, typename T2> SQRESULT callEventResult(R* r, const SQChar *eventName, T1 p1, T2 p2) {
+		return self.callMethodResult(r, eventName, p1, p2);
+	}
+
 };
 
 };// namespace
