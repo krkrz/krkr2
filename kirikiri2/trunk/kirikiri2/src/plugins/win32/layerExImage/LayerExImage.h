57e42839c42e14095f50ed5d1b80b164fc596308
wtnbgo <go@dsl.gr.jp>
2009-07-22 04:11:39 +0000

    LayerExImage ・VS2005に切り替え ・HSL調整用の moudulate() メソッドを追加
    git-svn-id: svn://localhost@4511 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/plugins/win32/layerExImage/LayerExImage.h
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/layerExImage/LayerExImage.h
@@ -1,14 +1,17 @@
 #ifndef __LayerExImage__
 #define __LayerExImage__
 
-#include "LayerExBase.h"
+#include "../layerExDraw/LayerExBase.hpp"
 
 /**
  * レイヤ拡張 イメージ操作用ネイティブインスタンス
  */
-class NI_LayerExImage : public NI_LayerExBase
+class layerExImage : public layerExBase
 {
 public:
+	// コンストラクタ
+	layerExImage(DispatchT obj) : layerExBase(obj) {}
+
 	/**
 	 * ルックアップテーブル反映
 	 * @param pLut lookup table
@@ -23,13 +26,21 @@ public:
 	void light(int brightness, int contrast);
 
 	/**
-	 * 色相と彩度
+	 * 色相と彩度を適応
 	 * @param hue 色相
 	 * @param sat 彩度
 	 * @param blend ブレンド 0 (効果なし) ～ 1 (full effect)
 	 */
 	void colorize(int hue, int sat, double blend);
 
+	/**
+	 * 色相と彩度と輝度調整
+	 * @param hue 色相 -180～180 (度)
+	 * @param saturation 彩度 -100～100 (%)
+	 * @param luminance 輝度 -100～100 (%)
+	 */
+	void modulate(int hue, int saturation, int luminance);
+	
 	/**
 	 * ノイズ追加
 	 * @param level ノイズレベル 0 (no noise) ～ 255 (lot of noise).
