b65b81af73b2a208ec1da5bc595dc5614320dc41
miahmie <miahmie@users.noreply.github.com>
2009-11-18 11:57:12 +0000

    shellExecute: ・バックグラウンドでコンソール出力つきコマンド実行を追加 ・メッセージの受け渡しをregisterMessageRecieverの代わりに専用メッセージウィンドウを使用するように調整 ・テストスクリプト追加
    git-svn-id: svn://localhost@4621 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- kirikiri2/trunk/kirikiri2/src/plugins/win32/shellExecute/main.cpp
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/shellExecute/main.cpp
@@ -3,9 +3,192 @@
 #include <process.h>
 #include "ncbind/ncbind.hpp"
 
-#define WM_SHELLEXECUTED (WM_APP+1)
+#define WM_SHELLEXECUTED   (WM_APP+1)
+#define WM_SHELLCONSOLEOUT (WM_APP+2)
 
 #define WSO_LOOPTIMEOUT 100
+
+/**
+ * コンソールコマンド用共通
+ */
+
+struct CommandExecute
+{
+	typedef void (*LineCallbackT)(void*, int, LPCWSTR);
+
+	HANDLE hOR, hOW, hIR, hIW, hEW;
+	PROCESS_INFORMATION pi;
+	DWORD exitcode;
+	bool timeouted;
+	enum { ERR_NONE, ERR_PIPE, ERR_PROC, ERR_TOUT, ERR_WAIT } error;
+
+	~CommandExecute() {
+		if (hOR) ::CloseHandle(hOR);
+		if (hOW) ::CloseHandle(hOW);
+		if (hIR) ::CloseHandle(hIR);
+		if (hIW) ::CloseHandle(hIW);
+		if (hEW) ::CloseHandle(hEW);
+		if (pi.hThread)  ::CloseHandle(pi.hThread);
+		if (pi.hProcess) ::CloseHandle(pi.hProcess);
+	}
+
+	CommandExecute()
+		: hOR(0), hOW(0), hIR(0), hIW(0), hEW(0), exitcode(~0L), timeouted(false), error(ERR_NONE)
+	{
+		::ZeroMemory(&pi, sizeof(pi));
+
+		// セキュリティ属性
+		SECURITY_ATTRIBUTES sa;
+		SECURITY_DESCRIPTOR sd;
+		::ZeroMemory(&sa, sizeof(sa));
+		sa.nLength= sizeof(sa);
+		sa.lpSecurityDescriptor = NULL;
+		sa.bInheritHandle = TRUE;
+
+		// NT系の場合はセキュリティ記述子も
+		OSVERSIONINFO osv;
+		::ZeroMemory(&osv, sizeof(osv));
+		osv.dwOSVersionInfoSize = sizeof(osv);
+		::GetVersionEx(&osv);
+		if (osv.dwPlatformId == VER_PLATFORM_WIN32_NT) {
+			::ZeroMemory(&sd, sizeof(sd));
+			::InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION);
+			::SetSecurityDescriptorDacl(&sd, true, NULL, false);
+			sa.lpSecurityDescriptor = &sd;
+		}
+
+		// パイプを作成
+		HANDLE hOT=0, hIT=0;
+		HANDLE hPID = ::GetCurrentProcess();
+		if (!(::CreatePipe(&hOT, &hOW, &sa,0) &&
+			  ::CreatePipe(&hIR, &hIT, &sa,0) &&
+			  ::DuplicateHandle(hPID, hOW, hPID, &hEW, 0,  TRUE, DUPLICATE_SAME_ACCESS) &&
+			  ::DuplicateHandle(hPID, hOT, hPID, &hOR, 0, FALSE, DUPLICATE_SAME_ACCESS) &&
+			  ::DuplicateHandle(hPID, hIT, hPID, &hIW, 0, FALSE, DUPLICATE_SAME_ACCESS)
+			  )) {
+			error = ERR_PIPE;
+		}
+		::CloseHandle(hOT);
+		::CloseHandle(hIT);
+	}
+
+	bool start(ttstr const &target, ttstr const &param) {
+		if (hasError()) return false;
+
+		ttstr cmd(L"\"");
+		// 吉里吉里サーチパス上にある場合はそちらを優先
+		if (TVPIsExistentStorage(target)) {
+			ttstr tmp = TVPGetPlacedPath(target);
+			TVPGetLocalName(tmp);
+			/**/cmd += tmp    + L"\"";
+		} else  cmd += target + L"\"";
+
+		if (param.length() > 0) cmd += L" " + param;
+		LPWSTR cmdline = (LPWSTR)cmd.c_str();
+
+		// 子プロセス作成
+		STARTUPINFO si;
+		::ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(si);
+		si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+		si.hStdOutput = hOW;
+		si.hStdInput  = hIR;
+		si.hStdError  = hEW;
+		si.wShowWindow = SW_HIDE;
+		if (!::CreateProcessW(0, cmdline, 0, 0, TRUE, CREATE_NEW_CONSOLE, 0, 0, &si, &pi)) {
+			error = ERR_PROC;
+			return false;
+		}
+		return true;
+	}
+
+	bool wait(LineCallbackT linecb, void *cbdata, int timeout = 0, DWORD cycle = WSO_LOOPTIMEOUT) {
+		// パイプから出力を読み込み
+		ttstr output;
+		DWORD cnt, last=::GetTickCount();
+		::PeekNamedPipe(hOR, 0, 0, 0, &cnt, NULL);
+		char buf[1024], crlf=0;
+		bool rest = false;
+		int line = 0;
+
+		if (!hasError()) while (true) {
+			if (cnt > 0) {
+				last = GetTickCount();
+				::ReadFile(hOR, buf, sizeof(buf)-1, &cnt, NULL);
+				buf[cnt] = 0;
+				DWORD start = 0;
+				for (DWORD pos = 0; pos < cnt; pos++) {
+					char ch = buf[pos];
+					if (ch == '\r' || ch == '\n') {
+						if (crlf == 0 || crlf == ch) {
+							buf[pos] = 0;
+							ttstr append(buf+start);
+							output += append;
+							linecb(cbdata, line++, output.c_str());
+							output.Clear();
+							buf[pos] = ch;
+							crlf = 0;
+						}
+						if (crlf) crlf = 0;
+						else crlf = ch;
+						start = pos+1;
+					} else {
+						crlf = 0;
+					}
+				}
+				if ((rest = (start < cnt))) {
+					ttstr append(buf+start);
+					output += append;
+				}
+				if ((int)cnt == sizeof(buf)-1) {
+					::PeekNamedPipe(hOR, 0, 0, 0, &cnt, NULL);
+					if (cnt > 0) continue;
+				}
+			} else {
+				if (timeout > 0 && ::GetTickCount() > last+timeout) {
+					::TerminateProcess(pi.hProcess, -1);
+					error = ERR_TOUT;
+					timeouted = true;
+				}
+			}
+			DWORD wait = ::WaitForSingleObject(pi.hProcess, cycle);
+			if (wait == WAIT_FAILED) {
+				error = ERR_WAIT;
+				break;
+			}
+			::PeekNamedPipe(hOR, 0, 0, 0, &cnt, NULL);
+			if (cnt == 0 && wait == WAIT_OBJECT_0) {
+				::GetExitCodeProcess(pi.hProcess, &exitcode);
+				break;
+			}
+		}
+		if (rest) linecb(cbdata, line++, output.c_str());
+
+		return hasError();
+	}
+
+	bool hasError() const { return (error != ERR_NONE); }
+	LPCWSTR getLastError() const {
+		switch (error) {
+		case ERR_PIPE: return L"can't create/duplicate pipe";
+		case ERR_PROC: return L"can't create child process";
+		case ERR_TOUT: return L"child process timeout";
+		case ERR_WAIT: return L"child process wait failed";
+		}
+		return L"";
+	}
+
+	DWORD getExitCode(bool *err = 0, bool *tout = 0, LPCWSTR *errmes = 0) const {
+		if (err )   *err    = hasError();
+		if (tout)   *tout   = timeouted;
+		if (errmes) *errmes = getLastError();
+		return exitcode;
+	}
+
+	HANDLE getProcessHandle() const { return pi.hProcess; }
+};
+
+
 
 /**
  * メソッド追加用クラス
@@ -14,40 +197,77 @@ class WindowShell {
 
 protected:
 	iTJSDispatch2 *objthis; //< オブジェクト情報の参照
+	HWND msgHWND;
 
+	typedef WindowShell SelfClass;
+#define MSGWND_CLASSNAME  L"Window ShellExecute Message Window Class"
+#define MSGWND_WINDOWNAME L"Window ShellExecute Message"
+	static ATOM MessageWindowClass;
+	HWND  createMessageWindow() {
+		HINSTANCE hinst = ::GetModuleHandle(NULL);
+		if (!MessageWindowClass) {
+			WNDCLASSEXW wcex = {
+				/*size*/sizeof(WNDCLASSEX), /*style*/0, /*proc*/WndProc, /*extra*/0L,0L, /*hinst*/hinst,
+				/*icon*/NULL, /*cursor*/NULL, /*brush*/NULL, /*menu*/NULL,
+				/*class*/MSGWND_CLASSNAME, /*smicon*/NULL };
+			if (!(MessageWindowClass = ::RegisterClassExW(&wcex)))
+				TVPThrowExceptionMessage(TJS_W("register window class failed."));
+		}
+		HWND hwnd = ::CreateWindowExW(0, (LPCWSTR)MAKELONG(MessageWindowClass, 0), MSGWND_WINDOWNAME,
+									  0, 0, 0, 1, 1, HWND_MESSAGE, NULL, hinst, NULL);
+		if (!hwnd) TVPThrowExceptionMessage(TJS_W("create message window failed."));
+		::SetWindowLong(hwnd, GWL_USERDATA, (LONG)this);
+		return hwnd;
+	}
+	HWND destroyMessageWindow(HWND hwnd) {
+		if (hwnd) {
+			::SetWindowLong(hwnd, GWL_USERDATA, 0);
+			::DestroyWindow(hwnd);
+		}
+		return NULL;
+	}
+	static LRESULT WINAPI WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+		SelfClass *self = (SelfClass*)(::GetWindowLong(hwnd, GWL_USERDATA));
+		if (self) switch (msg) {
+		case WM_SHELLEXECUTED:
+			self->onShellExecuted(wp, lp);
+			return 0;
+		case WM_SHELLCONSOLEOUT:
+			self->onCommandLineOutput(wp, lp);
+			return 0;
+		}
+		return DefWindowProc(hwnd, msg, wp, lp);
+	}
+public:
+	static void UnregisterMessageWindowClass() {
+		if (MessageWindowClass != 0 && ::UnregisterClassW((LPCWSTR)MAKELONG(MessageWindowClass, 0), ::GetModuleHandle(NULL)))
+			MessageWindowClass = 0;
+	}
+
+protected:
 	// イベント処理
-	static bool __stdcall shellExecuted(void *userdata, tTVPWindowMessage *Message) {
-		if (Message->Msg == WM_SHELLEXECUTED) {
-			iTJSDispatch2 *obj = (iTJSDispatch2*)userdata;
-			tTJSVariant process = (tjs_int)Message->WParam;
-			tTJSVariant endCode = (tjs_int)Message->LParam;
-			tTJSVariant *p[] = {&process, &endCode};
-			obj->FuncCall(0, L"onShellExecuted", NULL, NULL, 2, p, obj);
-			return true;
-		}
-		return false;
+	void onShellExecuted(WPARAM wp, LPARAM lp) {
+		tTJSVariant process = (tjs_int)wp;
+		tTJSVariant endCode = (tjs_int)lp;
+		tTJSVariant *p[] = {&process, &endCode};
+		objthis->FuncCall(0, L"onShellExecuted", NULL, NULL, 2, p, objthis);
 	}
-	
-	// ユーザメッセージレシーバの登録/解除
-	void setReceiver(tTVPWindowMessageReceiver receiver, bool enable) {
-		tTJSVariant mode     = enable ? (tTVInteger)(tjs_int)wrmRegister : (tTVInteger)(tjs_int)wrmUnregister;
-		tTJSVariant proc     = (tTVInteger)(tjs_int)receiver;
-		tTJSVariant userdata = (tTVInteger)(tjs_int)objthis;
-		tTJSVariant *p[] = {&mode, &proc, &userdata};
-		if (objthis->FuncCall(0, L"registerMessageReceiver", NULL, NULL, 4, p, objthis) != TJS_S_OK) {
-			TVPThrowExceptionMessage(L"can't regist user message receiver");
-		}
+	void onCommandLineOutput(WPARAM wp, LPARAM lp) {
+		tTJSVariant process = (tjs_int)wp;
+		tTJSVariant text = ttstr((LPCWSTR)lp);
+		tTJSVariant *p[] = { &process, &text };
+		objthis->FuncCall(0, L"onCommandLineOutput", NULL, NULL, 2, p, objthis);
 	}
-	
+
 public:
 	// コンストラクタ
 	WindowShell(iTJSDispatch2 *objthis) : objthis(objthis) {
-		setReceiver(shellExecuted, true);
+		msgHWND = createMessageWindow();
 	}
 
 	// デストラクタ
 	~WindowShell() {
-		setReceiver(shellExecuted, false);
+		msgHWND = destroyMessageWindow(msgHWND);
 	}
 
 public:
@@ -55,9 +275,11 @@ public:
 	 * 実行情報
 	 */
 	struct ExecuteInfo {
-		HANDLE process;         // 待ち対象プロセス
-		iTJSDispatch2 *objthis; // this 保持用
-		ExecuteInfo(HANDLE process, iTJSDispatch2 *objthis) : process(process), objthis(objthis) {};
+		HWND   message; // メッセージ送信先
+		HANDLE process; // 待ち対象プロセス
+		CommandExecute *cmd;
+		ExecuteInfo(HWND message, HANDLE process, CommandExecute *cmd = 0)
+			: message(message), process(process), cmd(cmd) {}
 	};
 	
 	/**
@@ -66,8 +288,8 @@ public:
 	 */
 	static void waitProcess(void *data) {
 		// パラメータ引き継ぎ
-		HANDLE process         = ((ExecuteInfo*)data)->process;
-		iTJSDispatch2 *objthis = ((ExecuteInfo*)data)->objthis;
+		HWND   message = ((ExecuteInfo*)data)->message;
+		HANDLE process = ((ExecuteInfo*)data)->process;
 		delete data;
 
 		// プロセス待ち
@@ -77,14 +299,12 @@ public:
 		CloseHandle(process);
 
 		// 送信
-		tTJSVariant val;
-		objthis->PropGet(0, TJS_W("HWND"), NULL, &val, objthis);
-		PostMessage(reinterpret_cast<HWND>((tjs_int)(val)), WM_SHELLEXECUTED, (WPARAM)process, (LPARAM)dt);
+		PostMessage(message, WM_SHELLEXECUTED, (WPARAM)process, (LPARAM)dt);
 	}
 	
 	/**
 	 * プロセスの停止
-	 * @param keyName 識別キー
+	 * @param process プロセスID
 	 * @param endCode 終了コード
 	 */
 	void terminateProcess(int process, int endCode) {
@@ -93,7 +313,6 @@ public:
 
 	/**
 	 * プロセスの実行
-	 * @param keyName 識別キー
 	 * @param target ターゲット
 	 * @praam param パラメータ
 	 */
@@ -107,13 +326,66 @@ public:
 		si.nShow = SW_SHOWNORMAL;
 		si.fMask = SEE_MASK_FLAG_NO_UI | SEE_MASK_NOCLOSEPROCESS;
 		if (ShellExecuteEx(&si)) {
-			_beginthread(waitProcess, 0, new ExecuteInfo(si.hProcess, objthis));
+			_beginthread(waitProcess, 0, new ExecuteInfo(msgHWND, si.hProcess));
 			return (int)si.hProcess;
 		}
 		return (int)INVALID_HANDLE_VALUE;
 	}
 
+
+	/**
+	 * commandExec コンソール処理
+	 */
+	struct ConsoleOutputParam {
+		HWND message;
+		HANDLE process;
+		ConsoleOutputParam(HWND message, HANDLE process) : message(message), process(process) {}
+		static void consoleLineOutCallback(void *v, int line, LPCWSTR text) {
+			ConsoleOutputParam *self = (ConsoleOutputParam*)v;
+			if (self) SendMessage(self->message, WM_SHELLCONSOLEOUT, (WPARAM)self->process, (LPARAM)text);
+		}
+	};
+	/**
+	 * commandExec 終了待ちスレッド処理
+	 * @param data ユーザ(ExecuteInfo)
+	 */
+	static void waitCommand(void *data) {
+		// パラメータ引き継ぎ
+		HWND        message = ((ExecuteInfo*)data)->message;
+		HANDLE      process = ((ExecuteInfo*)data)->process;
+		CommandExecute *cmd = ((ExecuteInfo*)data)->cmd;
+		delete data;
+
+		ConsoleOutputParam prm(message, process);
+		cmd->wait(ConsoleOutputParam::consoleLineOutCallback, (void*)&prm, 0);
+		DWORD exit = cmd->getExitCode();
+		delete cmd;
+
+		// 送信
+		PostMessage(message, WM_SHELLEXECUTED, (WPARAM)process, (LPARAM)exit);
+	}
+	/**
+	 * コマンドラインの実行
+	 * @param target ターゲット
+	 * @praam param パラメータ
+	 */
+	int commandExecute(ttstr target, ttstr param) {
+		CommandExecute *cmd = new CommandExecute();
+		if (cmd->start(target, param)) {
+			HANDLE proc = cmd->getProcessHandle();
+			_beginthread(waitCommand, 0, new ExecuteInfo(msgHWND, proc, cmd));
+			return (int)proc;
+		}
+		delete cmd;
+		return (int)INVALID_HANDLE_VALUE;
+	}
+
 };
+
+ATOM WindowShell::MessageWindowClass = 0;
+static void PostUnregistCallback() { WindowShell::UnregisterMessageWindowClass(); }
+NCB_POST_UNREGIST_CALLBACK(PostUnregistCallback);
+
 
 // インスタンスゲッタ
 NCB_GET_INSTANCE_HOOK(WindowShell)
@@ -131,7 +403,14 @@ NCB_GET_INSTANCE_HOOK(WindowShell)
 // フックつきアタッチ
 NCB_ATTACH_CLASS_WITH_HOOK(WindowShell, Window) {
 	Method(L"shellExecute", &WindowShell::shellExecute);
+	Method(L"commandExecute", &WindowShell::commandExecute);
 	Method(L"terminateProcess", &WindowShell::terminateProcess);
+}
+
+
+static void cmdExecLineCallback(void *va, int line, LPCWSTR text) {
+	iTJSDispatch2 *array = (iTJSDispatch2*)va;
+	array->PropSetByNum(TJS_MEMBERENSURE, line, &tTJSVariant(text), array);
 }
 
 /**
@@ -148,155 +427,33 @@ tjs_error TJS_INTF_METHOD commandExecute(
 
 	// コマンドライン/タイムアウト取得
 	int timeout = 0;
-	ttstr output, cmd(L"\""), target(param[0]->GetString());
+	ttstr target(param[0]->GetString()), cmdprm;
 
-	// 吉里吉里サーチパス上にある場合はそちらを優先
-	if (TVPIsExistentStorage(target)) {
-		target = TVPGetPlacedPath(target);
-		TVPGetLocalName(target);
-	}
-	cmd += target + L"\"";
-	if (numparams > 1) cmd += L" " + ttstr(param[1]->GetString());
+	if (numparams > 1) cmdprm  = param[1]->GetString();
 	if (numparams > 2) timeout = (tjs_int)*param[2];
 
-	LPWSTR cmdline = (LPWSTR)cmd.c_str();
-	tjs_char const *errmes = 0;
-	bool timeouted = false;
+	DWORD exit = ~0L;
+	bool haserr = true, timeouted = false;
+	LPCWSTR errmes = 0;
 
-	// セキュリティ属性
-	SECURITY_ATTRIBUTES sa;
-	SECURITY_DESCRIPTOR sd;
-	ZeroMemory(&sa, sizeof(sa));
-	sa.nLength= sizeof(sa);
-	sa.lpSecurityDescriptor = NULL;
-	sa.bInheritHandle = TRUE;
-
-	// NT系の場合はセキュリティ記述子も
-	OSVERSIONINFO osv;
-	ZeroMemory(&osv, sizeof(osv));
-	osv.dwOSVersionInfoSize = sizeof(osv);
-	GetVersionEx(&osv);
-	if (osv.dwPlatformId == VER_PLATFORM_WIN32_NT) {
-		ZeroMemory(&sd, sizeof(sd));
-		InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION);
-		SetSecurityDescriptorDacl(&sd, true, NULL, false);
-		sa.lpSecurityDescriptor = &sd;
-	}
-
-	// パイプを作成
-	HANDLE hOR=0, hOW=0, hIR=0, hIW=0, hEW=0;
-	HANDLE hOT=0, hIT=0;
-	HANDLE hPID = GetCurrentProcess();
-	if (!(CreatePipe(&hOT, &hOW, &sa,0) &&
-		  CreatePipe(&hIR, &hIT, &sa,0) &&
-		  DuplicateHandle(hPID, hOW, hPID, &hEW, 0,  TRUE, DUPLICATE_SAME_ACCESS) &&
-		  DuplicateHandle(hPID, hOT, hPID, &hOR, 0, FALSE, DUPLICATE_SAME_ACCESS) &&
-		  DuplicateHandle(hPID, hIT, hPID, &hIW, 0, FALSE, DUPLICATE_SAME_ACCESS)
-		  )) {
-		errmes = L"can't create/duplicate pipe";
-	}
-	CloseHandle(hOT);
-	CloseHandle(hIT);
-	if (errmes) goto error;
-
-	// 子プロセス作成
-	PROCESS_INFORMATION pi;
-	STARTUPINFO si;
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
-	si.hStdOutput = hOW;
-	si.hStdInput  = hIR;
-	si.hStdError  = hEW;
-	si.wShowWindow = SW_HIDE;
-	if (!CreateProcessW(0, cmdline, 0, 0, TRUE, CREATE_NEW_CONSOLE, 0, 0, &si, &pi)) {
-		errmes = L"can't create child process";
-		goto error;
-	}
-
-	// パイプから出力を読み込み
-	DWORD cnt, exit=~0L, last=GetTickCount();
-	PeekNamedPipe(hOR, 0, 0, 0, &cnt, NULL);
-	char buf[1024], crlf=0;
-	bool rest = false;
-	tjs_int32 line = 0;
 	iTJSDispatch2 *array = TJSCreateArrayObject();
+	if (array != 0) {
+		CommandExecute exec;
+		if (exec.start(target, cmdprm)) exec.wait(cmdExecLineCallback, array, timeout);
+		exit = exec.getExitCode(&haserr, &timeouted, &errmes);
 
-	while (true) {
-		if (cnt > 0) {
-			last = GetTickCount();
-			ReadFile(hOR, buf, sizeof(buf)-1, &cnt, NULL);
-			buf[cnt] = 0;
-			DWORD start = 0;
-			for (DWORD pos = 0; pos < cnt; pos++) {
-				char ch = buf[pos];
-				if (ch == '\r' || ch == '\n') {
-					if (crlf == 0 || crlf == ch) {
-						buf[pos] = 0;
-						ttstr append(buf+start);
-						output += append;
-						array->PropSetByNum(TJS_MEMBERENSURE, line++, &tTJSVariant(output), array);
-						output.Clear();
-						buf[pos] = ch;
-						crlf = 0;
-					}
-					if (crlf) crlf = 0;
-					else crlf = ch;
-					start = pos+1;
-				} else {
-					crlf = 0;
-				}
-			}
-			if ((rest = (start < cnt))) {
-				ttstr append(buf+start);
-				output += append;
-			}
-			if ((int)cnt == sizeof(buf)-1) {
-				PeekNamedPipe(hOR, 0, 0, 0, &cnt, NULL);
-				if (cnt > 0) continue;
-			}
-		} else {
-			if (timeout > 0 && GetTickCount() > last+timeout) {
-				TerminateProcess(pi.hProcess, -1);
-				errmes = L"child process timeout";
-				timeouted = true;
-			}
-		}
-		DWORD wait = WaitForSingleObject(pi.hProcess, WSO_LOOPTIMEOUT);
-		if (wait == WAIT_FAILED) {
-			errmes = L"child process wait failed";
-			break;
-		}
-		PeekNamedPipe(hOR, 0, 0, 0, &cnt, NULL);
-		if (cnt == 0 && wait == WAIT_OBJECT_0) {
-			GetExitCodeProcess(pi.hProcess, &exit);
-			break;
-		}
+		vStdOut.SetObject(array, array);
+		array->Release();
 	}
-	if (rest)
-		array->PropSetByNum(TJS_MEMBERENSURE, line++, &tTJSVariant(output), array);
-
-	vStdOut.SetObject(array, array);
-	array->Release();
-
-	CloseHandle(pi.hThread);
-	CloseHandle(pi.hProcess);
-
-error:
-	CloseHandle(hOR);
-	CloseHandle(hOW);
-	CloseHandle(hIR);
-	CloseHandle(hIW);
-	CloseHandle(hEW);
 
 	ncbDictionaryAccessor ret;
 	if (ret.IsValid()) {
 		ret.SetValue(L"stdout", vStdOut);
-		if (errmes != 0) {
+		if (haserr && errmes != 0) {
 			ret.SetValue(L"status", ttstr(timeouted ? L"timeout" : L"error"));
 			ret.SetValue(L"message", ttstr(errmes));
 		} else {
-			ret.SetValue(L"status", ttstr(L"ok"));
+			ret.SetValue(L"status", ttstr(haserr ? L"failed" : L"ok"));
 			ret.SetValue(L"exitcode", (tjs_int)exit);
 		}
 	}
