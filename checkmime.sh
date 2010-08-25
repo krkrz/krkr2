14e22540fbcddd0e1739329eff0f7d387dd27604
w-dee <w-dee@users.noreply.github.com>
2010-08-25 06:19:21 +0000

    mime/charset判定スクリプトを改良(nkfでエンコーディングを判定するように)
    git-svn-id: svn://localhost/kirikiri2/trunk@4846 131d44d2-bf85-024a-9602-49a0ea93a7a9

--- checkmime.sh
+++ checkmime.sh
@@ -1,35 +1,36 @@
 #! /bin/sh
 
-function check()
-{
-	svn list -R | while read n; do
-		if [ "x`svn propget svn:mime-type $n`" = "x" ]; then
-			MIMETYPE=""
-			case $n in
-				*.h)
-					MIMETYPE="text/x-chdr; charset=Shift_JIS"
-					;;
-				*.cpp)
-					MIMETYPE="text/x-c++src; charset=Shift_JIS"
-					;;
-				*.c)
-					MIMETYPE="text/x-csrc; charset=Shift_JIS"
-					;;
-				*.txt)
-					MIMETYPE="text/plain; charset=Shift_JIS"
-					;;
-				*.tjs)
-					MIMETYPE="text/x-tjs; charset=Shift_JIS"
-					;;
-				*.ks)
-					MIMETYPE="text/x-kag-scenario; charset=Shift_JIS"
-					;;
-			esac
-			if [ "x$MIMETYPE" != "x" ]; then
-					echo "svn propset \"svn:mime-type\" \"$MIMETYPE\" \"$n\""
-			fi
+svn list -R | while read n; do
+	if [ "x`svn propget svn:mime-type $n`" = "x" ]; then
+		MIMETYPE=""
+		case $n in
+			*.h)
+				MIMETYPE="text/x-chdr"
+				;;
+			*.cpp)
+				MIMETYPE="text/x-c++src"
+				;;
+			*.c)
+				MIMETYPE="text/x-csrc"
+				;;
+			*.txt)
+				MIMETYPE="text/plain"
+				;;
+			*.tjs)
+				MIMETYPE="text/x-tjs"
+				;;
+			*.ks)
+				MIMETYPE="text/x-kag-scenario"
+				;;
+			*.htm|*.html)
+				MIMETYPE="text/html"
+				;;
+		esac
+		if [ "x$MIMETYPE" != "x" ]; then
+			CHARSET=`nkf --guess $n`
+			MIMETYPE="$MIMETYPE; charset=$CHARSET"
+			echo "svn propset \"svn:mime-type\" \"$MIMETYPE\" \"$n\""
 		fi
-	done
-}
+	fi
+done
 
-check
