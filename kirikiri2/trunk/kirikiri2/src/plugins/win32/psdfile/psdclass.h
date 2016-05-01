27f7d28be91db0fe5030fe74cc659f54f531ecd9
wtnbgo <go@dsl.gr.jp>
2016-05-01 05:22:09 +0000

    クラス構造整理＆ソース分離
    git-svn-id: svn://localhost@5333 131d44d2-bf85-024a-9602-49a0ea93a7a9

---
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/psdfile/psdclass.h
@@ -0,0 +1,208 @@
+#ifndef __PSDCLASS_H__
+#define __PSDCLASS_H__
+
+#include <tp_stub.h>
+#include "psdparse/psdfile.h"
+
+class PSDStorage;
+
+class PSD : public psd::PSDFile
+{
+friend class PSDStorage;
+	
+public:
+	/**
+	 * コンストラクタ
+	 */
+	PSD(iTJSDispatch2 *objthis);
+
+	/**
+	 * デストラクタ
+	 */
+	~PSD();
+
+	/**
+	 * インスタンス生成ファクトリ
+	 */
+	static tjs_error factory(PSD **result, tjs_int numparams, tTJSVariant **params, iTJSDispatch2 *objthis);
+
+	/**
+	 * 生成時の自己オブジェクトを取得
+	 */
+	tTJSVariant getSelf();
+	
+	/**
+	 * PSD画像のロード
+	 * @param filename ファイル名
+	 * @return ロードに成功したら true
+	 */
+	bool load(ttstr filename);
+
+	static void clearStorageCache();
+	
+#define INTGETTER(tag) int get_ ## tag(){ return isLoaded ? header.tag : -1; }
+
+	INTGETTER(width);
+	INTGETTER(height);
+	INTGETTER(channels);
+	INTGETTER(depth);
+  int get_color_mode()  { return isLoaded ? header.mode : -1; }
+  int get_layer_count() { return isLoaded ? (int)layerList.size() : -1; }
+
+public:
+	/**
+	 * レイヤ種別の取得
+	 * @param no レイヤ番号
+	 * @return レイヤ種別
+	 */
+	int getLayerType(int no);
+
+	/**
+	 * レイヤ名称の取得
+	 * @param no レイヤ番号
+	 * @return レイヤ種別
+	 */
+	ttstr getLayerName(int no);
+
+	/**
+	 * レイヤ情報の取得
+	 * @param no レイヤ番号
+	 * @return レイヤ情報が格納された辞書
+	 */
+	tTJSVariant getLayerInfo(int no);
+
+	/**
+	 * レイヤデータの読み出し(内部処理)
+	 * @param layer 読み出し先レイヤ
+	 * @param no レイヤ番号
+     * @param imageMode イメージモード
+	 */
+  void _getLayerData(tTJSVariant layer, int no, psd::ImageMode imageMode);
+
+	/**
+	 * レイヤデータの読み出し
+	 * @param layer 読み出し先レイヤ
+	 * @param no レイヤ番号
+	 */
+	void getLayerData(tTJSVariant layer, int no);
+
+	/**
+	 * レイヤデータの読み出し(生イメージ)
+	 * @param layer 読み出し先レイヤ
+	 * @param no レイヤ番号
+	 */
+	void getLayerDataRaw(tTJSVariant layer, int no);
+
+	/**
+	 * レイヤデータの読み出し(マスクのみ)
+	 * @param layer 読み出し先レイヤ
+	 * @param no レイヤ番号
+	 */
+	void getLayerDataMask(tTJSVariant layer, int no);
+
+	/**
+	 * スライスデータの読み出し
+	 * @return スライス情報辞書 %[ top, left, bottom, right, slices:[ %[ id, group_id, left, top, bottom, right ], ... ] ]
+	 *         スライス情報がない場合は void を返す
+	 */
+	tTJSVariant getSlices();
+
+	/**
+	 * ガイドデータの読み出し
+	 * @return ガイド情報辞書 %[ vertical:[ x1, x2, ... ], horizontal:[ y1, y2, ... ] ]
+	 *         ガイド情報がない場合は void を返す
+	 */
+	tTJSVariant getGuides();
+
+	/**
+	 * 合成結果の取得。取得領域は画像全体サイズ内におさまってる必要があります
+   * 注意：PSDファイル自体に合成済み画像が存在しない場合は取得に失敗します
+   *
+	 * @param layer 格納先レイヤ(width,heightサイズに調整される)
+	 * @return 取得に成功したら true
+	 */
+  bool getBlend(tTJSVariant layer);
+
+	/**
+	 * レイヤーカンプ
+	 */
+	tTJSVariant getLayerComp();
+
+protected:
+	iTJSDispatch2 *objthis; ///< 自己オブジェクト情報の参照
+	ttstr dname; ///< 登録用ベース名
+
+	HGLOBAL hBuffer; // オンメモリ保持用ハンドル
+	bool loadMemory(const ttstr &filename);
+	void clearMemory();
+
+	/**
+	 * 内包データの消去
+	 */
+	void clearData();
+	
+	/**
+	 * レイヤ番号が適切かどうか判定
+	 * @param no レイヤ番号
+	 */
+	void checkLayerNo(int no);
+
+	/**
+	 * 名前の取得
+	 * @param layレイヤ情報
+	 */
+	static ttstr layname(psd::LayerInfo &lay);
+	
+	// ------------------------------------------------------------
+	// ストレージレイヤ参照用インターフェース
+	// ------------------------------------------------------------
+	
+protected:
+
+	// ストレージ情報登録
+	void addToStorage(const ttstr &filename);
+	void removeFromStorage();
+
+	bool storageStarted; //< ストレージ用の情報初期化済みフラグ
+
+	// レイヤ名を返す
+	static ttstr path_layname(psd::LayerInfo &lay);
+
+	// レイヤのパス名を返す
+	static ttstr pathname(psd::LayerInfo &lay);
+
+	// ストレージ処理用データの初期化
+	void startStorage();
+
+	/*
+	 * 指定した名前のレイヤの存在チェック
+	 * @param name パスを含むレイヤ名
+	 * @param layerIdxRet レイヤインデックス番号を返す
+	 */
+	bool CheckExistentStorage(const ttstr &filename, int *layerIdxRet=0);
+
+	/*
+	 * 指定したパスにあるファイル名一覧の取得
+	 * @param pathname パス名
+	 * @param lister リスト取得用インターフェース
+	 */
+	void GetListAt(const ttstr &pathname, iTVPStorageLister *lister);
+
+	/*
+	 * 指定した名前のレイヤの画像ファイルをストリームで返す
+	 * @param name パスを含むレイヤ名
+	 * @return ファイルストリーム
+	 */
+	IStream *openLayerImage(const ttstr &name);
+	
+	// パス名記録用
+
+	typedef std::map<int,int> LayerIdIdxMap; // layerId とレイヤ情報インデックスのマップ
+	LayerIdIdxMap layerIdIdxMap;
+
+	typedef std::map<ttstr,int> NameIdxMap;     //< レイヤ名とlayerId のマップ
+	typedef std::map<ttstr,NameIdxMap> PathMap; //< パス別のレイヤ名一覧
+	PathMap pathMap;
+};
+
+#endif
---
+++ kirikiri2/trunk/kirikiri2/src/plugins/win32/psdfile/psdclass_loadmem.cpp
@@ -0,0 +1,54 @@
+#include "psdclass.h"
+#include "psdparse/psdparse.h"
+
+void
+PSD::clearMemory()
+{
+	if (hBuffer) {
+		::GlobalUnlock(hBuffer);
+		::GlobalFree(hBuffer);
+		hBuffer = 0;
+	}
+}
+
+bool
+PSD::loadMemory(const ttstr &filename)
+{
+	// ‚Ü‚é‚²‚Æƒƒ‚ƒŠ‚É“Ç‚Ýž‚ñ‚Åˆ—
+	isLoaded = false;
+	IStream *stream = TVPCreateIStream(filename, TJS_BS_READ);
+	if (stream) {
+		try {
+			// ‘S•”ƒƒ‚ƒŠ‚É“Ç‚Ýž‚Þ
+			STATSTG stat;
+			stream->Stat(&stat, STATFLAG_NONAME);
+			tjs_uint64 qsize = (tjs_uint64)stat.cbSize.QuadPart;
+			if (qsize < 0xFFFFFFFF) {
+				DWORD size = (DWORD)qsize;
+				hBuffer = ::GlobalAlloc(GMEM_MOVEABLE, size);
+				unsigned char* pBuffer = (unsigned char*)::GlobalLock(hBuffer);
+				if (pBuffer) {
+					ULONG retsize;
+					stream->Read(pBuffer, size, &retsize);
+					psd::Parser<unsigned char*> parser(*this);
+					unsigned char *begin = pBuffer;
+					unsigned char *end   = begin + size;
+					bool r = parse(begin , end,  parser);
+					if (r && begin == end) {
+						dprint("succeeded\n");
+						isLoaded = processParsed();
+					}
+					if (!isLoaded) {
+						clearMemory();
+					}
+				}
+			}
+		} catch(...) {
+			clearMemory();
+			stream->Release();
+			throw;
+		}
+		stream->Release();
+	}
+	return isLoaded;	
+}
